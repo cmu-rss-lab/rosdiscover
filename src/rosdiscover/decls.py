eacd9d524975612ad091a02a61317b5045e0f1d2
ChrisTimperley <christimperley@googlemail.com>
2018-10-15 13:50:53 -0400

    added Declaration class

--- src/rosdiscover/decls.py
+++ src/rosdiscover/decls.py
@@ -1,15 +1,19 @@
 import attr
 
 
+class Declaration(object):
+    pass
+
+
 @attr.s(frozen=True)
-class NodeInit(object):
+class NodeInit(Declaration):
     name = attr.ib(type=str)
     package = attr.ib(type=str)
     defined_in_file = attr.ib(type=str)
 
 
 @attr.s(frozen=True)
-class ParamRead(object):
+class ParamRead(Declaration):
     name = attr.ib(type=str)
     package = attr.ib(type=str)
     defined_in_file = attr.ib(type=str)
