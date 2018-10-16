b57a71826b78fe5d9485d3670f6808f2eae15637
ChrisTimperley <christimperley@googlemail.com>
2018-10-16 14:54:53 -0400

    added model for joint_state_publisher

---
+++ src/rosdiscover/models/__init__.py
@@ -0,0 +1,1 @@
+from . import joint_state_publisher
---
+++ src/rosdiscover/models/joint_state_publisher.py
@@ -0,0 +1,27 @@
+from ..vm import model
+
+
+@model('joint_state_publisher', 'joint_state_publisher')
+def joint_state_publisher(c):
+    # https://github.com/ros/joint_state_publisher/blob/kinetic-devel/joint_state_publisher/joint_state_publisher/joint_state_publisher
+
+    # joint_state_publisher#L33
+    def get_param(name, value=None):
+        private = "~{}".format(name)
+        return c.read(private, read(name, value))
+
+    get_param("robot_description")
+    get_param("dependent_joints", {})
+    get_param("use_mimic_tags", True)
+    get_param("use_smallest_joint_limits", True)
+    get_param("zeros")
+
+    get_param("publish_default_positions", True)
+    get_param("publish_default_velocities", False)
+    get_param("publish_default_efforts", False)
+    get_param("use_gui", False)
+
+    for source in get_param("source_list", []):
+        c.sub(source, 'sensor_msgs/JointState')
+
+    c.pub('joint_states', 'sensor_msgs/JointState')
