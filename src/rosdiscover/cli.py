6d24cf507bce492976836bc1a06c8ab5c694e067
ChrisTimperley <christimperley@googlemail.com>
2018-10-22 14:49:53 -0400

    added launch stub to CLI

--- src/rosdiscover/cli.py
+++ src/rosdiscover/cli.py
@@ -9,12 +9,18 @@ logger = logging.getLogger(__name__)  # type: logging.Logger
 logger.setLevel(logging.DEBUG)
 
 
+def launch(fn):
+    # type: (str) -> None:
+    logger.info("simulating launch: %s", fn)
+
+
 def main():
     log_to_stdout = logging.StreamHandler()
     log_to_stdout.setLevel(logging.DEBUG)
     logging.getLogger('rosdiscover').addHandler(log_to_stdout)
 
-
     # simulates the architectural effects of a ROS launch
     parser = argparse.ArgumentParser(description=DESC)
-    parser.add_argument('filename', help='a ROS launch file')
+    parser.add_argument('filename', type=str, help='a ROS launch file')
+    args = parser.parse_args()
+    launch(args.filename)
