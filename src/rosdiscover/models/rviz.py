8b6f79f5b296d055026ec0c9d20645896e518994
ChrisTimperley <christimperley@googlemail.com>
2018-10-22 22:26:17 -0400

    added placeholder rviz

---
+++ src/rosdiscover/models/rviz.py
@@ -0,0 +1,10 @@
+from ..vm import model
+
+
+@model('rviz', 'rviz')
+def rviz(c):
+    c.provide('reload_shaders', 'std_srvs/Empty')
+
+    # https://github.com/ros-visualization/rviz/blob/070835c426b8982e304b38eb4a9c6eb221155d5f/src/rviz/default_plugin/marker_array_display.cpp
+    # FIXME
+    c.sub('visualization_marker_array', 'visualization_msgs/MarkerArray')
