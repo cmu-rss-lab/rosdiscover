e642fac36d79ba345408577fe578b2295cce6c69
ChrisTimperley <christimperley@googlemail.com>
2018-10-22 21:01:59 -0400

    fixed velocity smoother model

--- src/rosdiscover/models/velocity_smoother.py
+++ src/rosdiscover/models/velocity_smoother.py
@@ -1,22 +1,20 @@
 from ..vm import model
 
 
-@model('yocs_velocity_smoother', 'velocity_smoother')
+@model('yocs_velocity_smoother', 'VelocitySmootherNodelet')
 def velocity_smoother(c):
-    # FIXME how to private node handle?
-
-    c.read("frequency", 20.0)
-    c.read("decel_factor", 1.0)
-    c.read("robot_feedback", 0)
+    c.read("~frequency", 20.0)
+    c.read("~decel_factor", 1.0)
+    c.read("~robot_feedback", 0)
 
     # FIXME must be present!
-    # c.read("speed_lim_v")
-    # c.read("speed_lim_w")
-    # c.read("accel_lim_v")
-    # c.read("accel_lim_w")
+    # c.read("~speed_lim_v")
+    # c.read("~speed_lim_w")
+    # c.read("~accel_lim_v")
+    # c.read("~accel_lim_w")
 
-    c.sub("odometry", 'nav_msgs/Odometry')
-    c.sub("robot_cmd_vel", 'geometry_msgs/Twist')
-    c.sub("raw_cmd_vel", 'geometry_msgs/Twist')
+    c.sub("~odometry", 'nav_msgs/Odometry')
+    c.sub("~robot_cmd_vel", 'geometry_msgs/Twist')
+    c.sub("~raw_cmd_vel", 'geometry_msgs/Twist')
 
-    c.pub("smooth_cmd_vel", "geometry_msgs/Twist")
+    c.pub("~smooth_cmd_vel", "geometry_msgs/Twist")
