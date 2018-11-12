3c432a2f80be3bdb0f95a1321544bd9628478868
ChrisTimperley <christimperley@googlemail.com>
2018-11-12 14:39:33 -0500

    added docstring to models module

--- src/rosdiscover/models/__init__.py
+++ src/rosdiscover/models/__init__.py
@@ -1,3 +1,32 @@
+"""
+This module provides architectural models for various ROS packages.
+
+Each file within this directory defines a single model for a particular ROS
+node type. For now, models are implemented as Python functions that accept a
+single argument, which is used to supply a :code:`NodeContext` for evaluating
+the various architectural commands within the model definition (e.g.,
+subscribing to a particular topic).
+The :code:`model` decorator must be used to indicate that a particular
+function is used to supply a model definition, as shown below.
+
+.. code:: python
+
+    from ..interpreter import model
+
+    @register('amcl', 'amcl')
+    def amcl(c):
+        ...
+
+Note that the module (i.e., file) responsible for defining a particular model
+must be imported below in order for that model to be loaded. For example, to
+ensure that the model provided by :code:`joint_state_publisher.py` is loaded,
+the following :code:`import` statement must be added to this file:
+
+.. code:: python
+
+    from . import joint_state_publisher
+
+"""
 from . import joint_state_publisher
 from . import robot_state_publisher
 from . import map_server
