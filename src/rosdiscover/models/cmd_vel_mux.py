4e9a2cb1496e0941c606482e00d7985fdd223ece
Chris Timperley <christimperley@googlemail.com>
2018-11-12 15:48:10 -0500

    Implemented stageros model (#12)
    * added all pub/sub definitions for stageros
    
    * add FIXME note
    
    * implemented IO handling for cmd_vel_mxu

--- src/rosdiscover/models/cmd_vel_mux.py
+++ src/rosdiscover/models/cmd_vel_mux.py
@@ -1,32 +1,20 @@
+import yaml
+
 from ..interpreter import model
 
 
 @model('yocs_cmd_vel_mux', 'CmdVelMuxNodelet')
 def cmd_vel_mux(c):
     # FIXME handle IO
-    # fn = c.read("~yaml_cfg_file")
+    fn = c.read("~yaml_cfg_file")
 
-    # TODO FILE MUST EXIST
+    # TODO ensure that file exists
+    with open(fn, 'r') as f:
+        yml = yaml.load(f)
 
-    # FIXME based on contents of a file
-    name_output = "output_{}"
-
-    # publishers and subscribers
-    """
-    for (unsigned int i = 0; i < cmd_vel_sub.size(); i++)
-    {
-      cmd_vel_sub[i].subs =
-          nh_priv.subscribe<geometry_msgs::Twist>(cmd_vel_sub[i].topic, 10, CmdVelFunctor(i, this));
-
-      // Create (stopped by now) a one-shot timer for every subscriber
-      cmd_vel_sub[i].timer =
-          nh_priv.createTimer(ros::Duration(cmd_vel_sub[i].timeout), TimerFunctor(i, this), true, false);
-
-      NODELET_DEBUG("CmdVelMux : subscribed to '%s' on topic '%s'. pr: %d, to: %.2f",
-                cmd_vel_sub[i].name.c_str(), cmd_vel_sub[i].topic.c_str(),
-                cmd_vel_sub[i].priority, cmd_vel_sub[i].timeout);
-    }
-    """
+    c.pub(yml['publisher'], 'geometry_msgs/Twist')
+    for sub_desc in yml['subscribers']:
+        c.sub(sub_desc['topic'], 'geometry_msgs/Twist')
 
     c.pub("active", "std_msgs/String")
 
--- src/rosdiscover/models/stage_ros.py
+++ src/rosdiscover/models/stage_ros.py
@@ -5,10 +5,30 @@ from ..interpreter import model
 def stage_ros(c):
     c.write('/use_sim_time', True)
 
-    c.provide('reset_positions', 'std_srvs/Empty')
+    # FIXME implement mapName logic
+    # for now, this only supports a single robot and assumes that omitRobotID
+    # is true
 
-    # create pub/subs for each robot
-    # - for each robot, create lasers
-    # 'sensor_msgs/LaserScan'
+    # n_.advertise<nav_msgs::Odometry>(mapName(ODOM, r, static_cast<Stg::Model*>(new_robot->positionmodel)), 10);
+    c.pub('/odom', 'nav_msgs/Odometry')
+    # n_.advertise<nav_msgs::Odometry>(mapName(BASE_POSE_GROUND_TRUTH, r, static_cast<Stg::Model*>(new_robot->positionmodel)), 10);
+    c.pub('/base_pose_ground_truth', 'nav_msgs/Odometry')
+
+    # new_robot->cmdvel_sub = n_.subscribe<geometry_msgs::Twist>(mapName(CMD_VEL, r, static_cast<Stg::Model*>(new_robot->positionmodel)), 10, boost::bind(&StageNode::cmdvelReceived, this, r, _1));
+    c.sub('/cmd_vel', 'geometry_msgs/Twist')
+
+    # for (size_t s = 0; s < new_robot->lasermodels.size(); ++s)
+    # new_robot->laser_pubs.push_back(n_.advertise<sensor_msgs::LaserScan>(mapName(BASE_SCAN, r, static_cast<Stg::Model*>(new_robot->positionmodel)), 10));
+    c.pub('/base_scan', 'sensor_msgs/LaserScan')
+
+    # for (size_t s = 0; s < new_robot->cameramodels.size(); ++s)
+    # new_robot->image_pubs.push_back(n_.advertise<sensor_msgs::Image>(mapName(IMAGE, r, static_cast<Stg::Model*>(new_robot->positionmodel)), 10));
+    # new_robot->depth_pubs.push_back(n_.advertise<sensor_msgs::Image>(mapName(DEPTH, r, static_cast<Stg::Model*>(new_robot->positionmodel)), 10));
+    # new_robot->camera_pubs.push_back(n_.advertise<sensor_msgs::CameraInfo>(mapName(CAMERA_INFO, r, static_cast<Stg::Model*>(new_robot->positionmodel)), 10));
+    c.pub('/image', 'sensor_msgs/Image')
+    c.pub('/depth', 'sensor_msgs/Image')
+    c.pub('/camera_info', 'sensor_msgs/CameraInfo')
 
     c.pub('/clock', 'rosgraph_msgs/Clock')
+
+    c.provide('reset_positions', 'std_srvs/Empty')
