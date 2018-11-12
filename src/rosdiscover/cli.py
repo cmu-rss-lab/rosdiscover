52f3d362e5b2e68f98f2aebec8337d550b37b3ab
Chris Timperley <christimperley@googlemail.com>
2018-11-12 16:37:48 -0500

    Implemented rostopic command (fixes #11) (#14)
    * added subparsers to CLI
    
    * updated usage instructions
    
    * implemented rostopic list
    
    * renamed rostopic to rostopic_list

--- src/rosdiscover/cli.py
+++ src/rosdiscover/cli.py
@@ -16,30 +16,56 @@ logger = logging.getLogger(__name__)  # type: logging.Logger
 logger.setLevel(logging.DEBUG)
 
 
-def launch(fn_launch, dir_workspace):
-    # type: (str, str) -> None:
-    """
-    Simulates the architectural effects of a `roslaunch` command.
-    """
+def _launch(fn_launch, dir_workspace):
     logger.info("simulating launch [%s] inside workspace [%s]",
                 fn_launch, dir_workspace)
 
     workspace = Workspace(dir_workspace)
     interpreter = Interpreter(workspace)
     interpreter.launch(fn_launch)
+    return interpreter
 
+
+def launch(args):
+    """
+    Simulates the architectural effects of a `roslaunch` command.
+    """
+    interpreter = _launch(args.filename, args.workspace)
     output = [n.to_dict() for n in interpreter.nodes]
     print(yaml.dump(output, default_flow_style=False))
 
 
-def main():
-    log_to_stdout = logging.StreamHandler()
-    log_to_stdout.setLevel(logging.DEBUG)
-    logging.getLogger('rosdiscover').addHandler(log_to_stdout)
+def rostopic_list(args):
+    # simulates the list command
+    interpreter = _launch(args.filename, args.workspace)
+    topics = set()
+    for node in interpreter.nodes:
+        topics |= set(x for (x, _) in node.pubs | node.subs)
+    print('\n'.join(sorted(topics)))
 
-    # simulates the architectural effects of a ROS launch
+
+def main():
+    # log_to_stdout = logging.StreamHandler()
+    # log_to_stdout.setLevel(logging.DEBUG)
+    # logging.getLogger('rosdiscover').addHandler(log_to_stdout)
+
     parser = argparse.ArgumentParser(description=DESC)
-    parser.add_argument('filename', type=str, help='a ROS launch file')
-    parser.add_argument('--workspace', type=str, default='/ros_ws')
+    subparsers = parser.add_subparsers()
+
+    p = subparsers.add_parser(
+        'launch',
+        help='simulates the effects of a roslaunch.')
+    p.add_argument('filename', type=str, help='a ROS launch file')
+    p.add_argument('--workspace', type=str, default='/ros_ws')
+    p.set_defaults(func=launch)
+
+    p = subparsers.add_parser(
+        'rostopic',
+        help='simulates the output of rostopic for a given configuration.')
+    p.add_argument('filename', type=str, help='a ROS launch file')
+    p.add_argument('--workspace', type=str, default='/ros_ws')
+    p.set_defaults(func=rostopic_list)
+
     args = parser.parse_args()
-    launch(args.filename, args.workspace)
+    if 'func' in args:
+        args.func(args)
