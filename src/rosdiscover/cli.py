0ec23639c44c126f57bb2fbd491cda3501d30269
ChrisTimperley <christimperley@googlemail.com>
2018-11-12 16:34:17 -0500

    implemented rostopic list

--- src/rosdiscover/cli.py
+++ src/rosdiscover/cli.py
@@ -16,31 +16,38 @@ logger = logging.getLogger(__name__)  # type: logging.Logger
 logger.setLevel(logging.DEBUG)
 
 
-def launch(args):
-    """
-    Simulates the architectural effects of a `roslaunch` command.
-    """
-    fn_launch = args.filename
-    dir_workspace = args.workspace
+def _launch(fn_launch, dir_workspace):
     logger.info("simulating launch [%s] inside workspace [%s]",
                 fn_launch, dir_workspace)
 
     workspace = Workspace(dir_workspace)
     interpreter = Interpreter(workspace)
     interpreter.launch(fn_launch)
+    return interpreter
 
+
+def launch(args):
+    """
+    Simulates the architectural effects of a `roslaunch` command.
+    """
+    interpreter = _launch(args.filename, args.workspace)
     output = [n.to_dict() for n in interpreter.nodes]
     print(yaml.dump(output, default_flow_style=False))
 
 
 def rostopic(args):
-    logger.info("cool!")
+    # simulates the list command
+    interpreter = _launch(args.filename, args.workspace)
+    topics = set()
+    for node in interpreter.nodes:
+        topics |= set(x for (x, _) in node.pubs | node.subs)
+    print('\n'.join(sorted(topics)))
 
 
 def main():
-    log_to_stdout = logging.StreamHandler()
-    log_to_stdout.setLevel(logging.DEBUG)
-    logging.getLogger('rosdiscover').addHandler(log_to_stdout)
+    # log_to_stdout = logging.StreamHandler()
+    # log_to_stdout.setLevel(logging.DEBUG)
+    # logging.getLogger('rosdiscover').addHandler(log_to_stdout)
 
     parser = argparse.ArgumentParser(description=DESC)
     subparsers = parser.add_subparsers()
@@ -57,7 +64,7 @@ def main():
         help='simulates the output of rostopic for a given configuration.')
     p.add_argument('filename', type=str, help='a ROS launch file')
     p.add_argument('--workspace', type=str, default='/ros_ws')
-    p.set_defaults(fun=rostopic)
+    p.set_defaults(func=rostopic)
 
     args = parser.parse_args()
     if 'func' in args:
