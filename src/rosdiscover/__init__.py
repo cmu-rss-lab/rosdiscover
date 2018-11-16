d9aa2bda18b54b5302fb3fb26eb19ec964f2e65f
ChrisTimperley <christimperley@googlemail.com>
2018-11-15 19:49:36 -0500

    bug fix: fixed logging

--- src/rosdiscover/__init__.py
+++ src/rosdiscover/__init__.py
@@ -1,9 +1,9 @@
 import logging
+logger = logging.getLogger(__name__)  # type: logging.Logger
+logger.setLevel(logging.DEBUG)
+logging.basicConfig()
 
 from .version import __version__
 from .workspace import Workspace
 from .interpreter import Model, Interpreter
 from . import models
-
-logger = logging.getLogger(__name__)  # type: logging.Logger
-logger.setLevel(logging.DEBUG)
