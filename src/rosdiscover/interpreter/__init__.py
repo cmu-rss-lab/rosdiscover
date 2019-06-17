4453b6890cddcef126a065e99e6e4509fd564360
ChrisTimperley <christimperley@googlemail.com>
2019-06-17 18:44:23 -0400

    updated style

--- src/rosdiscover/interpreter/__init__.py
+++ src/rosdiscover/interpreter/__init__.py
@@ -186,13 +186,13 @@ class NodeContext(object):
 
 class Model(object):
     """Models the architectural interactions of a node type."""
-    _models = {}  # type: Dict[Tuple[str, str], Model]
+    _models: Dict[Tuple[str, str], Model] = {}
 
     @staticmethod
-    def register(package,       # type: str
-                 name,          # type: str
-                 definition     # type: Callable[[NodeContext], None]
-                 ):             # type: (...) -> None
+    def register(package: str,
+                 name: str,
+                 definition: Callable[[NodeContext], None]
+                 ) -> None:
         key = (package, name)
         models = Model._models
         if key in models:
@@ -204,8 +204,7 @@ class Model(object):
                      name, package)
 
     @staticmethod
-    def find(package, name):
-        # type: (str, str) -> Model
+    def find(package: str, name: str) -> Model:
         return Model._models[(package, name)]
 
     def __init__(self,
@@ -217,49 +216,37 @@ class Model(object):
         self.__name = name
         self.__definition = definition
 
-    def eval(self, context):
-        # type: (NodeContext) -> None
+    def eval(self, context: NodeContext) -> None:
         return self.__definition(context)
 
 
-def model(package, name):
-    # type: (str, str) -> Any
-    def register(m):
-        # type: (Callable[[NodeContext], None]) -> Any
+def model(package: str, name: str) -> Any:
+    def register(m: Callable[[NodeContext], None]) -> Any:
         Model.register(package, name, m)
         return m
     return register
 
 
 class Interpreter(object):
-    def __init__(self, workspace):
+    # FIXME use image
+    def __init__(self, workspace) -> None:
         # type: (Workspace) -> None
         self.__workspace = workspace
         self.__params = ParameterServer()
-        self.__nodes = set()  # type: Set[NodeSummary]
+        self.__nodes: Set[NodeSummary] = set()
 
     @property
-    def parameters(self):
-        """
-        The simulated parameter server for this interpreter.
-        """
-        # type: () -> ParameterServer
+    def parameters(self) -> ParameterServer:
+        """The simulated parameter server for this interpreter."""
         return self.__params
 
     @property
-    def nodes(self):
-        """
-        Returns an iterator over the summaries for each node on the ROS graph.
-        """
-        # type: () -> Iterator[NodeSummary]
-        for n in self.__nodes:
-            yield n
+    def nodes(self) -> Iterator[NodeSummary]:
+        """Returns an iterator of summaries for each ROS node."""
+        yield from self.__nodes
 
-    def launch(self, fn):
-        # type: (str) -> None
-        """
-        Simulates the effects of `roslaunch` using a given launch file.
-        """
+    def launch(self, fn: str) -> None:
+        """Simulates the effects of `roslaunch` using a given launch file."""
         config = roslaunch.config.ROSLaunchConfig()
         loader = roslaunch.xmlloader.XmlLoader()
         loader.load(fn, config)
@@ -282,23 +269,19 @@ class Interpreter(object):
                 logger.exception("failed to launch node: %s", node.name)
                 raise
 
-    def create_nodelet_manager(self, name):
-        # type: (str) -> None
-        """
-        Creates a nodelet manager with a given name.
-        """
+    def create_nodelet_manager(self, name: str) -> None:
+        """Creates a nodelet manager with a given name."""
         logger.info('launched nodelet manager: %s', name)
 
     def load_nodelet(self,
-                     pkg,           # type: str
-                     nodetype,      # type: str
-                     name,          # type: str
-                     namespace,     # type: str
-                     remappings,    # type: Dict[str, str]
-                     manager        # type: str
-                     ):             # type: (...) -> None
-        """
-        Loads a nodelet using the provided instructions.
+                     pkg: str,
+                     nodetype: str,
+                     name: str,
+                     namespace: str,
+                     remappings: Dict[str, str],
+                     manager: str
+                     ) -> None:
+        """Loads a nodelet using the provided instructions.
 
         Parameters:
             pkg: the name of the package to which the nodelet belongs.
@@ -318,15 +301,14 @@ class Interpreter(object):
         return self.load(pkg, nodetype, name, namespace, remappings, '')
 
     def load(self,
-             pkg,           # type: str
-             nodetype,      # type: str
-             name,          # type: str
-             namespace,     # type: str
-             remappings,    # type: Dict[str, str]
-             args           # type: str
-             ):             # type: (...) -> None
-        """
-        Loads a node using the provided instructions.
+             pkg: str,
+             nodetype: str,
+             name: str,
+             namespace: str,
+             remappings: Dict[str, str],
+             args: str
+             ) -> None:
+        """Loads a node using the provided instructions.
 
         Parameters:
             pkg: the name of the package to which the node belongs.
