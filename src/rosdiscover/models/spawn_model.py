1a3c6624b3f854e56337780523334fd03ac65bae
ChrisTimperley <christimperley@googlemail.com>
2019-07-29 15:59:48 -0400

    added spawn_model model

--- src/rosdiscover/models/spawn_model.py
+++ src/rosdiscover/models/spawn_model.py
@@ -3,4 +3,11 @@ from ..interpreter import model
 
 @model('gazebo_ros', 'spawn_model')
 def spawn_model(c):
-    return
+    ns_gz = '/gazebo'
+    c.sub(f'{ns_gz}/model_states', 'gazebo_msgs/ModelStates')
+    c.use(f'{ns_gz}/unpause_physics', 'std_srvs/Empty')
+    c.use(f'{ns_gz}/delete_model', 'gazebo_msgs/DeleteModel')
+    c.use(f'{ns_gz}/spawn_urdf_model_client', 'gazebo_msgs/SpawnModel')
+    c.use(f'{ns_gz}/spawn_sdf_model_client', 'gazebo_msgs/SpawnModel')
+    c.use(f'{ns_gz}/spawn_sdf_model_client', 'gazebo_msgs/SpawnModel')
+    c.use(f'{ns_gz}/set_model_configuration', 'gazebo_msgs/SetModelConfiguration')
