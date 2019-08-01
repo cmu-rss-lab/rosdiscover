a8ba2ddbfb0dd3a747e3a81a589a55cc2e7d752f
ChrisTimperley <christimperley@googlemail.com>
2019-08-01 12:58:47 -0400

    modelled diff_drive plugin

--- src/rosdiscover/models/turtlebot3_teleop_key.py
+++ src/rosdiscover/models/turtlebot3_teleop_key.py
@@ -3,5 +3,5 @@ from ..interpreter import model
 
 @model('turtlebot3_teleop', 'turtlebot3_teleop_key')
 def turtlebot3_teleop_key(c):
-    c.pub('cmd_vel', 'geometry/Twist')
+    c.pub('cmd_vel', 'geometry_msgs/Twist')
     model = c.read('model', 'burger')
