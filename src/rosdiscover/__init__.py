01ffc24962ad93db17cecf8917176937c0790316
ChrisTimperley <christimperley@googlemail.com>
2019-08-08 10:12:49 -0400

    tidying code

--- src/rosdiscover/__init__.py
+++ src/rosdiscover/__init__.py
@@ -4,6 +4,4 @@ from .version import __version__
 from .interpreter import Model, Interpreter
 from . import models
 
-
-# create a root logger for rosdiscover
 logging.getLogger(__name__).setLevel(logging.DEBUG)
