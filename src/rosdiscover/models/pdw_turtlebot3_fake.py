15673a644ea86b07adda94e6d6e6f41b03f05f6d
ChrisTimperley <christimperley@googlemail.com>
2019-09-30 11:43:08 -0400

    added turtlebot3_fake_node

---
+++ src/rosdiscover/models/pdw_turtlebot3_fake.py
@@ -0,0 +1,16 @@
+from ..interpreter import model
+
+
+@model('pdw_turtlebot3_fake', 'turtlebot3_fake_node')
+def turtlebot3_fake_node(c):
+    model = c.read('tb3_model', '')
+    c.read('wheel_left_joint_name', 'wheel_left_joint')
+    c.read('wheel_right_joint_name', 'wheel_right_joint')
+    c.read('joint_states_frame', 'base_footprint')
+    c.read('odom_frame', 'odom')
+    c.read('odom_frame', 'odom')
+    c.read('base_frame', 'base_footprint')
+
+    c.pub('joint_states', 'sensor_msgs/JointState')
+    c.pub('odom', 'nav_msgs/Odometry')
+    c.sub('cmd_vel', 'geometry_msgs/Twist')
