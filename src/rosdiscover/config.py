4192598024a155e77728e586c47c3a2699e180fd
ChrisTimperley <christimperley@googlemail.com>
2020-04-23 17:50:12 -0400

    added from_yaml_file

--- src/rosdiscover/config.py
+++ src/rosdiscover/config.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 __all__ = ('Config',)
 
-from typing import Sequence
+from typing import Any, Mapping, Sequence
 
 import attr
+import yaml
 
 
 @attr.s(frozen=True, slots=True, auto_attribs=True)
@@ -23,6 +24,42 @@ class Config:
     image: str
     sources: Sequence[str]
     launches: Sequence[str]
+
+    @classmethod
+    def from_dict(cls, dict_: Mapping[str, Any]) -> 'Config':
+        """
+        Raises
+        ------
+        ValueError
+            If 'image' is undefined in configuration.
+        ValueError
+            If 'launches' is undefined in configuration.
+        """
+        if 'image' not in dict_:
+            raise ValueError("'image' is undefined in configuration")
+        if 'launches' not in dict_:
+            raise ValueError("'launches' is undefined in configuration")
+        if 'sources' not in dict_:
+            raise ValueError("'sources' is undefined in configuration")
+
+        if not isinstance(dict_['image'], str):
+            raise ValueError("expected 'image' to be a string")
+        if not isinstance(dict_['sources'], list):
+            raise ValueError("expected 'sources' to be a list")
+        if not isinstance(dict_['launches'], list):
+            raise ValueError("expected 'launches' to be a list")
+
+        image: str = dict_['image'] 
+        sources: Sequence[str] = dict_['sources']
+        launches: Sequence[str] = dict_['launches']
+        return Config(image=image,
+                      sources=sources,
+                      launches=launches)
+
+    @classmethod
+    def from_yaml_file(cls, filename: str) -> 'Config':
+        dict_ = yaml.load(args.config, Loader=yaml.SafeLoader)
+        return cls.from_dict(dict_)
 
     def __attrs_post_init__(self) -> None:
         object.__setattr__(self, 'sources', tuple(self.sources))
