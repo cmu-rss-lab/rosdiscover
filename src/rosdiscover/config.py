37180eac657373da06891fc1551b9439d520ac0a
ChrisTimperley <christimperley@googlemail.com>
2020-04-23 17:42:33 -0400

    added launches to Config

--- src/rosdiscover/config.py
+++ src/rosdiscover/config.py
@@ -16,9 +16,14 @@ class Config:
     sources: Sequence[str]
         The sequence of source files that should be sourced to obtain the
         catkin workspace for the application.
+    launches: Sequence[str]
+        The sequence of launch files that should be used to launch the
+        application.
     """
     image: str
     sources: Sequence[str]
+    launches: Sequence[str]
 
     def __attrs_post_init__(self) -> None:
         object.__setattr__(self, 'sources', tuple(self.sources))
+        object.__setattr__(self, 'launches', tuple(self.launches))
