f43a79178ecb7c2be40b533543a9caf3757b619e
ChrisTimperley <christimperley@googlemail.com>
2020-04-30 15:55:52 -0400

    implemented locate_node_binary

--- src/rosdiscover/recover/__init__.py
+++ src/rosdiscover/recover/__init__.py
@@ -7,8 +7,11 @@ operates under fairly heavy restrictions and is most successful when applied
 to simple nodes with static models (rather than nodes that may publish and
 subscribe to different topics at run-time depending on their configuration).
 """
-from typing import Iterator
+from typing import Iterator, Optional
 import contextlib
+import os
+import shlex
+import subprocess
 
 from comby import Comby
 from loguru import logger
@@ -40,10 +43,59 @@ class RecoveryTool:
                          app: _roswire.System) -> 'RecoveryTool':
         return RecoveryTool(config=config, system=app)
 
+    def locate_node_binary(self, package: str, node: str) -> str:
+        """Attempts to locate the binary for a given node.
+
+        Returns
+        -------
+        str
+            The absolute path of the binary for that node.
+
+        Raises
+        ------
+        ValueError
+            If the given package could not be found.
+        ValueError
+            If no binary can be located for the given node.
+        """
+        path: Optional[str] = None
+        logger.debug(f'locating binary for node [{node}] '
+                     f'in package [{package}]')
+        shell = self._system.shell
+        files = self._system.files
+
+        packages = self._system.description.packages
+        if package not in packages:
+            raise ValueError(f"package not found: {package}")
+        package_dir = packages[package].path
+
+        # start by looking in libexec
+        command = ('catkin_find --first-only --libexec '
+                   f'{shlex.quote(package)} {shlex.quote(node)}')
+        try:
+            path = shell.check_output(command, stderr=False)
+        except subprocess.CalledProcessError as err:
+            pass
+
+        # look in the scripts directory of the package's source directory
+        path_in_scripts_dir = os.path.join(package_dir, 'scripts', node)
+        if files.isfile(path_in_scripts_dir):
+            if files.access(path_in_scripts_dir, os.X_OK):
+                path = path_in_scripts_dir
+
+        if not path:
+            m = (f"unable to locate binary for node [{node}] "
+                 f"in package [{package}]")
+            raise ValueError(m)
+
+        logger.debug(f'located binary for node [{node}] '
+                     f'in package [{package}]: {path}')
+        return path
+
     def recover_node(self, package: str, node: str) -> RecoveredNodeModel:
         logger.debug(f'recovering model for node [{node}] '
                      f'in package [{package}]')
-        # FIXME hardcoded
+        binary_path = self.locate_node_binary(package, node)
         return self.recover_py_node(package, node)
 
     def recover_cpp_node(self, package: str, node: str) -> RecoveredNodeModel:
