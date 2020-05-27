5fffe5b6379c86ae7440461943056f794482b728
ChrisTimperley <christimperley@googlemail.com>
2020-05-27 11:05:05 -0400

    removed redundant import

--- src/rosdiscover/__init__.py
+++ src/rosdiscover/__init__.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-import logging
 from .version import __version__
 from .interpreter import Model, Interpreter
 from . import models
-
-
-# create a root logger for rosdiscover
-logging.getLogger(__name__).setLevel(logging.DEBUG)
