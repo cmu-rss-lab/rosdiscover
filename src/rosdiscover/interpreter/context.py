0508b4e5fa6b272906f94b79276a35e405eba2c6
ChrisTimperley <christimperley@googlemail.com>
2020-06-23 21:36:02 -0400

    Updated NodeContext to use attrs

--- src/rosdiscover/interpreter/context.py
+++ src/rosdiscover/interpreter/context.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-from typing import Any, Dict, Mapping, Optional, Set, Tuple
+from typing import Any, Mapping, Optional, Set, Tuple
 
 from loguru import logger
+import attr
 import dockerblade
 import roswire.name as rosname
 
@@ -9,65 +10,50 @@ from .summary import NodeSummary
 from .parameter import ParameterServer
 
 
+@attr.s(slots=True, auto_attribs=True)
 class NodeContext:
-    def __init__(self,
-                 name: str,
-                 namespace: str,
-                 kind: str,
-                 package: str,
-                 args: str,
-                 remappings: Dict[str, str],
-                 launch_filename: str,
-                 params: ParameterServer,
-                 files: dockerblade.files.FileSystem,
-                 ) -> None:
-        assert rosname.name_is_legal(namespace)
-        namespace = rosname.global_name(namespace)
+    name: str
+    namespace: str
+    kind: str
+    package: str
+    args: str
+    remappings: Mapping[str, str]
+    launch_filename: str
+    _params: ParameterServer = attr.ib(repr=False)
+    _files: dockerblade.files.FileSystem = attr.ib(repr=False)
+    _nodelet: bool = attr.ib(default=False, repr=False)
+    _placeholder: bool = attr.ib(default=False, repr=False)
+    _uses: Set[Tuple[str, str]] = attr.ib(factory=set, repr=False)
+    _provides: Set[Tuple[str, str]] = attr.ib(factory=set, repr=False)
+    _subs: Set[Tuple[str, str]] = attr.ib(factory=set, repr=False)
+    _pubs: Set[Tuple[str, str]] = attr.ib(factory=set, repr=False)
+    _action_servers: Set[Tuple[str, str]] = attr.ib(factory=set, repr=False)
+    _action_clients: Set[Tuple[str, str]] = attr.ib(factory=set, repr=False)
+    # The tuple is (name, dynamic) where name is the name of the parameter
+    # and dynamic is whether the node reacts to updates to the parameter via reconfigure
+    _reads: Set[Tuple[str, bool]] = attr.ib(factory=set, repr=False)
+    _writes: Set[str] = attr.ib(factory=set, repr=False)
 
-        self.__name = name
-        self.__namespace = namespace
-        self.__kind = kind
-        self.__package = package
-        self.__params = params
-        self.__files = files
-        self.__args = args
-        self.__launch_filename = launch_filename
-        self.__nodelet: bool = False
-        self.__placeholder: bool = False
-        self.__uses: Set[Tuple[str, str]] = set()
-        self.__provides: Set[Tuple[str, str]] = set()
-        self.__subs: Set[Tuple[str, str]] = set()
-        self.__pubs: Set[Tuple[str, str]] = set()
-
-        self.__action_servers: Set[Tuple[str, str]] = set()
-        self.__action_clients: Set[Tuple[str, str]] = set()
-
-        # The tuple is (name, dynamic) where name is the name of the parameter
-        # and dynamic is whether the node reacts to updates to the parameter via reconfigure
-        self.__reads: Set[Tuple[str, bool]] = set()
-        self.__writes: Set[str] = set()
-
-        self.__remappings: Mapping[str, str] = {
+    def __attrs_post_init__(self) -> None:
+        assert rosname.name_is_legal(self.namespace)
+        self.namespace = rosname.global_name(self.namespace)
+        self.remappings = {
             self._resolve_without_remapping(x):
             self._resolve_without_remapping(y)
-            for (x, y) in remappings.items()
+            for (x, y) in self.remappings.items()
         }
 
     @property
-    def args(self) -> str:
-        return self.__args
-
-    @property
     def fullname(self) -> str:
-        ns = self.__namespace
+        ns = self.namespace
         if ns[-1] != '/':
             ns += ' /'
-        return f'{ns}{self.__name}'
+        return f'{ns}{self.name}'
 
     def _apply_remappings(self, name: str) -> str:
         """Applies any appropriate remappings to a fully qualified name."""
-        for remap_from in sorted(self.__remappings):
-            remap_to = self.__remappings[remap_from]
+        for remap_from in sorted(self.remappings):
+            remap_to = self.remappings[remap_from]
             if name.startswith(remap_from):
                 name_new = name.replace(remap_from, remap_to, 1)
                 logger.info(f"applying remapping from [{name}] to [{name_new}]")
@@ -75,22 +61,22 @@ class NodeContext:
         return name
 
     def summarize(self) -> NodeSummary:
-        return NodeSummary(name=self.__name,
+        return NodeSummary(name=self.name,
                            fullname=self.fullname,
-                           filename=self.__launch_filename,
-                           namespace=self.__namespace,
-                           kind=self.__kind,
-                           package=self.__package,
-                           nodelet=self.__nodelet,
-                           placeholder=self.__placeholder,
-                           reads=self.__reads,
-                           writes=self.__writes,
-                           pubs=self.__pubs,
-                           subs=self.__subs,
-                           provides=self.__provides,
-                           uses=self.__uses,
-                           action_servers=self.__action_servers,
-                           action_clients=self.__action_clients)
+                           filename=self.launch_filename,
+                           namespace=self.namespace,
+                           kind=self.kind,
+                           package=self.package,
+                           nodelet=self._nodelet,
+                           placeholder=self._placeholder,
+                           reads=self._reads,
+                           writes=self._writes,
+                           pubs=self._pubs,
+                           subs=self._subs,
+                           provides=self._provides,
+                           uses=self._uses,
+                           action_servers=self._action_servers,
+                           action_clients=self._action_clients)
 
     def _resolve_without_remapping(self, name: str) -> str:
         """Resolves a given name to a global name, without applying
@@ -103,7 +89,7 @@ class NodeContext:
             return f'{self.fullname}/{name[1:]}'
         # relative and base names
         else:
-            return rosname.namespace_join(self.__namespace, name)
+            return rosname.namespace_join(self.namespace, name)
 
     def resolve(self, name: str) -> str:
         """Resolves a given name within the context of this node.
@@ -122,41 +108,47 @@ class NodeContext:
 
     def provide(self, service: str, fmt: str) -> None:
         """Instructs the node to provide a service."""
-        logger.debug(f"node [{self.__name}] provides service [{service}] "
+        logger.debug(f"node [{self.name}] provides service [{service}] "
                      f"using format [{fmt}]")
         service_name_full = self.resolve(service)
-        self.__provides.add((service_name_full, fmt))
+        self._provides.add((service_name_full, fmt))
 
     def use(self, service: str, fmt: str) -> None:
         """Instructs the node to use a given service."""
-        logger.debug(f"node [{self.__name}] uses a service [{service}] "
+        logger.debug(f"node [{self.name}] uses a service [{service}] "
                      f"with format [{fmt}]")
         service_name_full = self.resolve(service)
-        self.__uses.add((service_name_full, fmt))
+        self._uses.add((service_name_full, fmt))
 
     def sub(self, topic_name: str, fmt: str) -> None:
         """Subscribes the node to a given topic.
 
-        Parameters:
-            topic: the unqualified name of the topic.
-            fmt: the message format used by the topic.
+        Parameters
+        ----------
+        topic_name: str
+            The unqualified name of the topic.
+        fmt: str
+            The name of message format used by the topic.
         """
         topic_name_full = self.resolve(topic_name)
-        logger.debug(f"node [{self.__name}] subscribes to topic "
+        logger.debug(f"node [{self.name}] subscribes to topic "
                      f"[{topic_name}] with format [{fmt}]")
-        self.__subs.add((topic_name_full, fmt))
+        self._subs.add((topic_name_full, fmt))
 
     def pub(self, topic_name: str, fmt: str) -> None:
         """Instructs the node to publish to a given topic.
 
-        Parameters:
-            topic: the unqualified name of the topic.
-            fmt: the message format used by the topic.
+        Parameters
+        ----------
+        topic_name: str
+            the unqualified name of the topic.
+        fmt: str
+            the message format used by the topic.
         """
         topic_name_full = self.resolve(topic_name)
-        logger.debug(f"node [{self.__name}] publishes to topic "
+        logger.debug(f"node [{self.name}] publishes to topic "
                      f"[{topic_name}] with format [{fmt}]")
-        self.__pubs.add((topic_name_full, fmt))
+        self._pubs.add((topic_name_full, fmt))
 
     def read(self,
              param: str,
@@ -164,32 +156,35 @@ class NodeContext:
              dynamic: bool = False
              ) -> Any:
         """Obtains the value of a given parameter from the parameter server."""
-        logger.debug(f"node [{self.__name}] reads parameter [{param}]")
+        logger.debug(f"node [{self.name}] reads parameter [{param}]")
         param = self.resolve(param)
-        self.__reads.add((param, dynamic))
-        return self.__params.get(param, default)
+        self._reads.add((param, dynamic))
+        return self._params.get(param, default)
 
     def write(self, param: str, val: Any) -> None:
-        logger.debug(f"node [{self.__name}] writes [{val}] to "
+        logger.debug(f"node [{self.name}] writes [{val}] to "
                      f"parameter [{param}]")
         param = self.resolve(param)
-        self.__writes.add(param)
+        self._writes.add(param)
 
     def read_file(self, fn: str) -> str:
         """Reads the contents of a text file."""
-        return self.__files.read(fn)
+        return self._files.read(fn)
 
     def action_server(self, ns: str, fmt: str) -> None:
         """Creates a new action server.
 
-        Parameters:
-            ns: the namespace of the action server.
-            fmt: the name of the action format used by the server.
+        Parameters
+        ----------
+        ns: str
+            the namespace of the action server.
+        fmt: str
+            the name of the action format used by the server.
         """
-        logger.debug(f"node [{self.__name}] provides action server "
+        logger.debug(f"node [{self.name}] provides action server "
                      f"[{ns}] with format [{fmt}]")
         ns = self.resolve(ns)
-        self.__action_servers.add((ns, fmt))
+        self._action_servers.add((ns, fmt))
 
         self.sub(f'{ns}/goal', f'{fmt}Goal')
         self.sub(f'{ns}/cancel', 'actionlib_msgs/GoalID')
@@ -199,14 +194,18 @@ class NodeContext:
 
     def action_client(self, ns: str, fmt: str) -> None:
         """Creates a new action client.
-        Parameters:
-            ns: the namespace of the corresponding action server.
-            fmt: the name of the action format used by the server.
+
+        Parameters
+        ----------
+        ns: str
+            the namespace of the corresponding action server.
+        fmt: str
+            the name of the action format used by the server.
         """
-        logger.debug(f"node [{self.__name}] provides action client "
+        logger.debug(f"node [{self.name}] provides action client "
                      f"[{ns}] with format [{fmt}]")
         ns = self.resolve(ns)
-        self.__action_clients.add((ns, fmt))
+        self._action_clients.add((ns, fmt))
 
         self.pub(f'{ns}/goal', f'{fmt}Goal')
         self.pub(f'{ns}/cancel', 'actionlib_msgs/GoalID')
@@ -214,8 +213,8 @@ class NodeContext:
         self.sub(f'{ns}/feedback', f'{fmt}Feedback')
         self.sub(f'{ns}/result', f'{fmt}Result')
 
-    def mark_nodelet(self):
-        self.__nodelet = True
+    def mark_nodelet(self) -> None:
+        self._nodelet = True
 
-    def mark_placeholder(self):
-        self.__placeholder = True
+    def mark_placeholder(self) -> None:
+        self._placeholder = True
