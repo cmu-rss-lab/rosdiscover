a7119f2d3142f6bd684e09ee7e557406f6060dd9
ChrisTimperley <christimperley@googlemail.com>
2020-06-23 22:57:34 -0400

    added from_xml_element method

--- src/rosdiscover/models/plugins/gazebo.py
+++ src/rosdiscover/models/plugins/gazebo.py
@@ -4,8 +4,18 @@ This file provides model plugins that represent various Gazebo plugins.
 """
 __all__ = ('GazeboPlugin',)
 
+import xml.etree.ElementTree as ET
+
+from loguru import logger
+
 from ..interpreter.plugin import ModelPlugin as _ModelPlugin
 
 
 class GazeboPlugin(_ModelPlugin):
     """Represents the architectural effects of a Gazebo plugin."""
+    @classmethod
+    def from_xml_element(cls, xml: ET.Element) -> 'GazeboPlugin':
+        name = xml.attrib['name']
+        filename = xml.attrib['filename']
+        logger.debug(f'loading gazebo plugin [{name}] from file [{filename}]')
+        raise NotImplementedError
