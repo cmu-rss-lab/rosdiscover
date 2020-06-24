4bcbbbed12feee7b6121d1dcae173ff9b9b5d746
Mehal Kashyap <mehalkashyap@gmail.com>
2020-06-24 16:23:50 -0400

    Added json, cleaned up code

--- src/rosdiscover/ros2_translator/arch.py
+++ src/rosdiscover/ros2_translator/arch.py
@@ -1,41 +1,38 @@
-import yaml
+import json
 from launch.actions import IncludeLaunchDescription
 from launch_ros.actions import Node
 from launch.launch_description_sources import get_launch_description_from_python_launch_file
-
-description = get_launch_description_from_python_launch_file('robot.launch.py')
-nodes = description.entities
-
 
 def toyaml(nodes):
     to_yaml = {}
     no_name = 0
     for node in nodes:
         if isinstance(node, IncludeLaunchDescription):
-            description1 = node._IncludeLaunchDescription__launch_description_source.try_get_launch_description_without_context()
-            if description1 is None:
+            description_from_node = node._IncludeLaunchDescription__launch_description_source.try_get_launch_description_without_context()
+            if description_from_node is None:
                 continue
-            nodes1 = description1.entities
+            nodes1 = description_from_node.entities
             to_yaml.update(toyaml(nodes1))
         if isinstance(node, Node):
             dic = {}
             if node._Node__node_name is not None:
                 name_entry = node._Node__node_name
-                dic.update({'name': node._Node__node_name})
+                dic['name'] = node._Node__node_name
             else:
-                name = 'unknown' + str(no_name)
-                name_entry = name
+                name_entry = 'unknown' + str(no_name)
                 no_name += 1
-                dic.update({'name': name})
-            if node._Node__node_namespace is not None:
-                dic.update({'namespace': node._Node__node_namespace})
-            if node._Node__node_executable is not None:
-                dic.update({'executable': node._Node__node_executable})
-            if node._Node__package is not None:
-                dic.update({'package': node._Node__package})
-            to_yaml.update({name_entry: dic})
+                dic['name'] = name_entry
+            if node._Node__node_namespace:
+                dic['namespace'] = node._Node__node_namespace
+            if node._Node__node_executable:
+                dic['executable'] = node._Node__node_executable
+            if node._Node__package:
+                dic['package'] =  node._Node__package
+            to_yaml[name_entry] = dic
     return to_yaml
 
-
-with open('Arch.yml', 'w') as f:
-    print(yaml.dump(toyaml(nodes), f, default_flow_style=False))
+if __name__ == '__main__':
+    description = get_launch_description_from_python_launch_file('robot.launch.py')
+    nodes = description.entities
+    with open('Arch.yml', 'w') as f:
+        f.write(json.dumps(toyaml(nodes), indent=4, separators=(". ", " = ")))
