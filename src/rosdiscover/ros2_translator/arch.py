affad9c1e87a1d57975fa488b06acfb9ed461c5a
Mehal Kashyap <mehalkashyap@gmail.com>
2020-06-24 13:48:19 -0400

    Style 2

--- src/rosdiscover/ros2_translator/arch.py
+++ src/rosdiscover/ros2_translator/arch.py
@@ -1,5 +1,4 @@
 import yaml
-from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch_ros.actions import Node
 from launch.launch_description_sources import get_launch_description_from_python_launch_file
@@ -14,19 +13,19 @@ def toyaml(nodes):
     for node in nodes:
         if isinstance(node, IncludeLaunchDescription):
             description1 = node._IncludeLaunchDescription__launch_description_source.try_get_launch_description_without_context()
-            if description1 == None:
+            if description1 is None:
                 continue
             nodes1 = description1.entities
-            to_yaml.update(toYaml(nodes1))
+            to_yaml.update(toyaml(nodes1))
         if isinstance(node, Node):
             dic = {}
             if node._Node__node_name is not None:
-                name = node._Node__node_name
-                dic.update({name+'name': node._Node__node_name})
+                name_entry = node._Node__node_name
+                dic.update({'name': node._Node__node_name})
             else:
                 name = 'unknown' + str(no_name)
                 name_entry = name
-                no_name+=1
+                no_name += 1
                 dic.update({'name': name})
             if node._Node__node_namespace is not None:
                 dic.update({'namespace': node._Node__node_namespace})
@@ -39,4 +38,4 @@ def toyaml(nodes):
 
 
 with open('Arch.yml', 'w') as f:
-    print(yaml.dump(toYaml(nodes), f, default_flow_style=False))
+    print(yaml.dump(toyaml(nodes), f, default_flow_style=False))
