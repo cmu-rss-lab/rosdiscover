540e04168a6d5c76296e4042d972ff083e8a70ed
Mehal Kashyap <mehalkashyap@gmail.com>
2020-06-25 12:49:06 -0400

    Style

--- src/rosdiscover/ros2_translator/arch.py
+++ src/rosdiscover/ros2_translator/arch.py
@@ -41,12 +41,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Find nodes')
     parser.add_argument('launchfile', type=str)
     parser.add_argument('--output', type=str)
+    args = parser.parse_args()
     if args.output:
         yaml_file = args.output
-    else: 
+    else:
         yaml_file = 'arch.yml'
-    args = parser.parse_args()
     description = get_launch_description_from_python_launch_file(args.launchfile)
     nodes = description.entities
-    with open(yml_file, 'w') as f:
+    with open(yaml_file, 'w') as f:
         f.write(json.dumps(toyaml(nodes), indent=4, separators=(". ", " = ")))
