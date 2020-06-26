c15b9d2448f5a30bbe67cb8cc9e8b646f89608aa
Mehal Kashyap <mehalkashyap@gmail.com>
2020-06-26 13:09:51 -0400

    Update to IncludeLaunchDesciption to load launch file

--- src/rosdiscover/ros2_translator/arch.py
+++ src/rosdiscover/ros2_translator/arch.py
@@ -1,52 +1,61 @@
-import json
+from typing import Any, Dict, List
 import argparse
+import json
+from launch import LaunchDescription, LaunchDescriptionEntity
+from launch.launch_context import LaunchContext
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import IncludeLaunchDescription
 from launch_ros.actions import Node
-from launch.launch_description_sources import get_launch_description_from_python_launch_file
-
-
-def toyaml(nodes):
-    to_yaml = {}
-    no_name = 0
-    for node in nodes:
-        if isinstance(node, IncludeLaunchDescription):
-            description_from_node = node._IncludeLaunchDescription__launch_description_source.try_get_launch_description_without_context()
-            if description_from_node is None:
-                continue
-            nodes1 = description_from_node.entities
-            to_yaml.update(toyaml(nodes1))
-        if isinstance(node, Node):
-            # Note: The code directly accesses variables since there are no getters for
-            # these specific variables in launch.
-            # This code might break with changed implementations of launch
-            dic = {}
-            if node._Node__node_name:
-                name_entry = node._Node__node_name
-                dic['name'] = node._Node__node_name
-            else:
-                name_entry = 'unknown' + str(no_name)
-                no_name += 1
-                dic['name'] = name_entry
-            if node._Node__node_namespace:
-                dic['namespace'] = node._Node__node_namespace
-            if node._Node__node_executable:
-                dic['executable'] = node._Node__node_executable
-            if node._Node__package:
-                dic['package'] = node._Node__package
-            to_yaml[name_entry] = dic
-    return to_yaml
-
-
+NodeDescription = Dict[str, Any]
+class NodeDescriptionExtractor:
+    def __init__(self) -> None:
+        self.__unnamed_nodes = 0
+    def _node_to_description(self, node: Node) -> NodeDescription:
+        # Note: The code directly accesses variables since there are no getters for
+        # these specific variables in launch.
+        # This code might break with changed implementations of launch
+        output: NodeDescription = {}
+        if node._Node__node_name:
+            name_entry = node._Node__node_name
+            output['name'] = node._Node__node_name
+        else:
+            name_entry = f'unknown{self.__unnamed_nodes}'
+            self.__unnamed_nodes += 1
+            output['name'] = name_entry
+        if node._Node__node_namespace:
+            output['namespace'] = node._Node__node_namespace
+        if node._Node__node_executable:
+            output['executable'] = node._Node__node_executable
+        if node._Node__package:
+            output['package'] = node._Node__package
+        return output
+    def _extract_descriptions(self,
+                              entity: LaunchDescriptionEntity,
+                              context: LaunchContext
+                              ) -> List[NodeDescription]:
+        descriptions: List[NodeDescription] = []
+        if isinstance(entity, LaunchDescription) or isinstance(entity, IncludeLaunchDescription):  # noqa
+            for sub_entity in entity.visit(context):
+                descriptions += self._extract_descriptions(sub_entity, context)
+        elif isinstance(entity, Node):
+            descriptions = [self._node_to_description(entity)]
+        else:
+            print(f'UNKNOWN ENTITY: {type(entity)}')
+        return descriptions
+    def extract(self, filename: str) -> List[NodeDescription]:
+        context = LaunchContext()
+        launch_description_source = \
+            PythonLaunchDescriptionSource(args.launchfile)
+        launch_description = LaunchDescription([
+            IncludeLaunchDescription(launch_description_source)
+        ])
+        return self._extract_descriptions(launch_description, context)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Find nodes')
     parser.add_argument('launchfile', type=str)
-    parser.add_argument('--output', type=str)
+    parser.add_argument('--output', type=str, default='arch.json')
     args = parser.parse_args()
-    if args.output:
-        yaml_file = args.output
-    else:
-        yaml_file = 'arch.yml'
-    description = get_launch_description_from_python_launch_file(args.launchfile)
-    nodes = description.entities
-    with open(yaml_file, 'w') as f:
-        f.write(json.dumps(toyaml(nodes), indent=4, separators=(". ", " = ")))
+    extractor = NodeDescriptionExtractor()
+    output = extractor.extract(args.launchfile)
+    with open(args.output, 'w') as f:
+        f.write(json.dumps(output, indent=2))
