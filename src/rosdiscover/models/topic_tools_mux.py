42d3e5f464763b5194ced7cd740669e1917f40c9
Bradley Schmerl <schmerl@cs.cmu.edu>
2020-07-17 10:55:50 -0400

    Added relay

--- src/rosdiscover/models/topic_tools_mux.py
+++ src/rosdiscover/models/topic_tools_mux.py
@@ -21,3 +21,21 @@ def mux(c):
     c.pub(topics.outopic, "any")
     for in_topic in topics.intopic:
         c.sub(in_topic, "any")
+
+@model("topic_tools", "relay")
+def relay(c):
+    c.read("~unreliable", False)
+    c.read('~lazy', False)
+    c.read('~stealth', False)
+    # TODO: Chris: how should we treat this? I think this means a topic is created
+    # that by default is the same is intopic, but could be set by a parameter
+    # http://wiki.ros.org/topic_tools/relay
+    c.read('~monitor_topic', 'intopic')
+    c.read('~monitor_rate', 1.0)
+
+    parser = argparse.ArgumentParser("topic_tools/relay")
+    parser.add_argument("intopic", type=str)
+    parser.add_argument("outtopic", type=str)
+    topics = parser.parse_args(c.args.split())
+    c.pub(topics.outtopic, 'any')
+    c.sub(topics.intopic, 'any')
