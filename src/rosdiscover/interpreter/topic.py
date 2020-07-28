82fd02800484a3bae8979888fe5375215a7e625b
schmerl <schmerl@cs.cmu.edu>
2020-07-28 15:10:19 -0400

    Fixed some style issues

--- src/rosdiscover/interpreter/topic.py
+++ src/rosdiscover/interpreter/topic.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 __all__ = ('Topic',)
+
 from typing import Any, Dict
+
 import attr
 
 
-@attr.s(frozen=True, slots=True)
+@attr.s(frozen=True, slots=True, auto_attribs=True)
 class Topic:
     """Describes a topic"""
-    name: str = attr.ib()
-    format: str = attr.ib()
-    implicit: bool = attr.ib()
+    name: str
+    format: str
+    implicit: bool
 
     def to_dict(self) -> Dict[str, Any]:
         return {'name': self.name, 'format': self.format, 'implicit': self.implicit}
