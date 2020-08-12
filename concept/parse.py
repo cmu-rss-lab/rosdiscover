36481e6f67197f0080d22148dad237053ba798e7
ChrisTimperley <christimperley@googlemail.com>
2020-08-12 10:58:53 -0400

    added handling of more AST types

--- concept/parse.py
+++ concept/parse.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
+#
+# tricky bits:
+# - where is a given name defined? [maintain a lookup table?]
+# - alias analysis!
+#
+# some sort of exotic visitor is _probably_ the best way to handle this
+from typing import List, Optional, Tuple
+
 import attr
 import parso
 
@@ -20,11 +28,13 @@ def main():
 
 def evaluate(node: parso.tree.NodeOrLeaf, env: Environment):
     # dispatch to appropriate method based on type
-    print(f"@ dispatching: {node.type}")
+    # print(f"@ dispatching: {node.type}")
     if node.type == 'file_input':
         return evaluate_module(node, env)
     elif node.type == 'simple_stmt':
         return evaluate_simple_stmt(node, env)
+    elif node.type == 'suite':
+        return evaluate_suite(node, env)
     elif node.type == 'import_name':
         return evaluate_import_name(node, env)
     elif node.type == 'import_from':
@@ -35,6 +45,8 @@ def evaluate(node: parso.tree.NodeOrLeaf, env: Environment):
         return evaluate_if_stmt(node, env)
     elif node.type in ('endmarker', 'newline'):
         return evaluate_noop(node, env)
+    elif node.type == 'power':
+        return evaluate_power(node, env)
     else:
         print(f"UNKNOWN TYPE: {node.type}")
 
@@ -43,17 +55,34 @@ def evaluate_noop(node: parso.tree.NodeOrLeaf, env: Environment):
     return
 
 
+def evaluate_power(node: parso.tree.NodeOrLeaf, env: Environment):
+    print(node)
+
+
 def evaluate_expr_stmt(node: parso.tree.NodeOrLeaf, env: Environment):
     print(f"attempting to evaluate expr stmt: {node}")
+    print(node.children)
 
 
 def evaluate_if_stmt(node: parso.tree.NodeOrLeaf, env: Environment):
-    for branch_condition_node in node.get_test_nodes():
-        print(f"evaluating branch condition: {branch_condition_node}")
+    # find each branch and its corresponding condition, if any.
+    branches: List[Tuple[parso.tree.NodeOrLeaf, Optional[parso.tree.NodeOrLeaf]]] = []
+    for index, child in enumerate(node.children):
+        if child in ('if', 'elif'):
+            condition = node.children[index + 1]
+            branch = node.children[index + 3]
+            branches.append((branch, condition))
+        elif child ==  'else':
+            branch = node.children[index + 2]
+            branches.append((branch, None))
 
+    for branch, condition in branches:
         # TODO attempt to evaluate the branch condition in the current
         # environment
+        print(f"evaluating branch condition: {condition}")
         is_satisfied = True
+        if is_satisfied:
+            evaluate(branch, env)
 
 
 def evaluate_import_name(node: parso.tree.NodeOrLeaf, env: Environment):
@@ -70,6 +99,11 @@ def evaluate_import_from(node: parso.tree.NodeOrLeaf, env: Environment):
 
 
 def evaluate_module(node: parso.tree.NodeOrLeaf, env: Environment):
+    for child in node.children:
+        evaluate(child, env)
+
+
+def evaluate_suite(node: parso.tree.NodeOrLeaf, env: Environment):
     for child in node.children:
         evaluate(child, env)
 
