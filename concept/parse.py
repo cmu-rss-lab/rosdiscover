cb0d46f0ed6e2d499038b42cb47e1604e20345e2
ChrisTimperley <christimperley@googlemail.com>
2020-08-12 10:23:00 -0400

    added Environment placeholder

--- concept/parse.py
+++ concept/parse.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
+import attr
 import parso
+
+
+@attr.s
+class Environment:
+    pass
 
 
 def main():
@@ -8,64 +14,69 @@ def main():
         file_contents = f.read()
 
     node_module = parso.parse(file_contents, version='2.7')
-    evaluate(node_module)
+    environment = Environment()
+    evaluate(node_module, environment)
 
 
-def evaluate(node: parso.tree.NodeOrLeaf):
+def evaluate(node: parso.tree.NodeOrLeaf, env: Environment):
     # dispatch to appropriate method based on type
     print(f"@ dispatching: {node.type}")
     if node.type == 'file_input':
-        return evaluate_module(node)
+        return evaluate_module(node, env)
     elif node.type == 'simple_stmt':
-        return evaluate_simple_stmt(node)
+        return evaluate_simple_stmt(node, env)
     elif node.type == 'import_name':
-        return evaluate_import_name(node)
+        return evaluate_import_name(node, env)
     elif node.type == 'import_from':
-        return evaluate_import_from(node)
+        return evaluate_import_from(node, env)
     elif node.type == 'expr_stmt':
-        return evaluate_expr_stmt(node)
+        return evaluate_expr_stmt(node, env)
     elif node.type == 'if_stmt':
-        return evaluate_if_stmt(node)
+        return evaluate_if_stmt(node, env)
     elif node.type in ('endmarker', 'newline'):
-        return evaluate_noop(node)
+        return evaluate_noop(node, env)
     else:
         print(f"UNKNOWN TYPE: {node.type}")
 
 
-def evaluate_noop(node: parso.tree.NodeOrLeaf):
+def evaluate_noop(node: parso.tree.NodeOrLeaf, env: Environment):
     return
 
 
-def evaluate_expr_stmt(node: parso.tree.NodeOrLeaf):
+def evaluate_expr_stmt(node: parso.tree.NodeOrLeaf, env: Environment):
     print(f"attempting to evaluate expr stmt: {node}")
 
 
-def evaluate_if_stmt(node: parso.tree.NodeOrLeaf):
+def evaluate_if_stmt(node: parso.tree.NodeOrLeaf, env: Environment):
     for branch_condition_node in node.get_test_nodes():
         print(f"evaluating branch condition: {branch_condition_node}")
 
+        # TODO attempt to evaluate the branch condition in the current
+        # environment
+        is_satisfied = True
 
-def evaluate_import_name(node: parso.tree.NodeOrLeaf):
+
+def evaluate_import_name(node: parso.tree.NodeOrLeaf, env: Environment):
     defined_names = [name.value for name in node.get_defined_names()]
     for module_name in defined_names:
         print(f"$ importing {module_name}")
 
 
-def evaluate_import_from(node: parso.tree.NodeOrLeaf):
+def evaluate_import_from(node: parso.tree.NodeOrLeaf, env: Environment):
     imported_names = [name.value for name in node.get_defined_names()]
     from_name = '.'.join(name.value for name in node.get_from_names())
     for module_name in imported_names:
         print(f"$ importing {module_name} from {from_name}")
 
 
-def evaluate_module(node: parso.tree.NodeOrLeaf):
+def evaluate_module(node: parso.tree.NodeOrLeaf, env: Environment):
     for child in node.children:
-        evaluate(child)
+        evaluate(child, env)
 
 
-def evaluate_simple_stmt(node: parso.tree.NodeOrLeaf):
+def evaluate_simple_stmt(node: parso.tree.NodeOrLeaf, env: Environment):
     for child in node.children:
-        evaluate(child)
+        evaluate(child, env)
 
 
 if __name__ == '__main__':
