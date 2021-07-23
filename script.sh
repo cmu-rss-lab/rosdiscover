2fe75f89cd49a3356bca7fb3d749621a9ac9d73f
schmerl <schmerl@cs.cmu.edu>
2021-07-23 15:29:39 -0400

    Put --no-binary at end?

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 echo "CRYPTOGRAPHY_DONT_BUILD_RUST = " $CRYPTOGRAPHY_DONT_BUILD_RUST
-python -m pip install --no-binary cryptography "$@"
+python -m pip install "$@" --no-binary cryptography
