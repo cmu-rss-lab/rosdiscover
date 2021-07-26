c17d45fcc9e8d919de4bd318aee1f0da9d3b1b88
Tobias Dürschmid <duerschmid@cmu.edu>
2021-07-26 15:12:30 -0400

    Improve the format checking (#135)

--- src/rosdiscover/config.py
+++ src/rosdiscover/config.py
@@ -58,11 +58,7 @@ class Config:
             raise ValueError("expected 'sources' to be a list")
         if not isinstance(dict_['launches'], list):
             raise ValueError("expected 'launches' to be a list")
-        if isinstance(dict_['launches'][0], dict):
-            launch_args_provided = True
-        elif isinstance(dict_['launches'][0], str):
-            launch_args_provided = False
-        else:
+        if not all(type(d) in (dict, str) for d in dict_["launches"]):
             raise ValueError("expected elements of 'launches' to be str or dict")
             
         has_environment = 'environment' in dict_
