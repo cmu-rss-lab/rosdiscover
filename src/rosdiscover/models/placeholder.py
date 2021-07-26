02891462c6e94ba0a4ab5823ae37b0518d26e778
schmerl <schmerl@cs.cmu.edu>
2021-07-26 10:52:55 -0400

    Introduce flake error to check that travis is working.

--- src/rosdiscover/models/placeholder.py
+++ src/rosdiscover/models/placeholder.py
@@ -4,4 +4,4 @@ from ..interpreter import model
 @model('PLACEHOLDER', 'PLACEHOLDER')
 def placeholder(c):
     # c.pub("/diagnostics", "diagnostic_msgs/DiagnosticArray")
-    c.mark_placeholder()
+     c.mark_placeholder()
