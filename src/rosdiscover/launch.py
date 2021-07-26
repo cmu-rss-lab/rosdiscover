e874c0a6921b0850e2f1faeda2bd4ea7b07220cf
Tobias Dürschmid <duerschmid@cmu.edu>
2021-07-26 10:19:35 -0400

    Add launch class (#135)

---
+++ src/rosdiscover/launch.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+__all__ = ('Launch',)
+
+from types import MappingProxyType
+from typing import Any, Mapping, Sequence
+
+from loguru import logger
+import attr
+import yaml
+
+@attr.s(frozen=True, slots=True, auto_attribs=True)
+class Launch:
+    """
+    Attributes
+    ----------
+    filename: str
+        The file name of the launch files that should be launched
+    arguments: Mapping[str, str]
+        A set of launch arguments that should be passed to roslaunch
+    """
+    filename: str
+    arguments: Mapping[str, str]
+
+    @classmethod
+    def from_dict(cls, dict_: Mapping[str, Any]) -> 'Launch':
+        """
+        Raises
+        ------
+        ValueError
+            If 'filename' is undefined in configuration.
+        """
+        if 'filename' not in dict_:
+            raise ValueError("'filename' is undefined in configuration")
+
+        if not isinstance(dict_['filename'], str):
+            raise ValueError("expected 'filename' to be a string")
+
+        has_arguments = 'arguments' in dict_
+        if has_arguments and not isinstance(dict_['arguments'], dict):
+            raise ValueError("expected 'arguments' to be a mapping")
+
+        filename: str = dict_['filename']
+        arguments: Mapping[str, str] = dict(dict_.get('arguments', {}))
+        return Launch(image=image,
+                      arguments=arguments)
+
+    def __attrs_post_init__(self) -> None:
+        object.__setattr__(self, 'filename', filename)
+        object.__setattr__(self, 'arguments', arguments)
