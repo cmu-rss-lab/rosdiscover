a62ca145794c8317942ea2e209c301b7ea5bc3c7
ChrisTimperley <christimperley@googlemail.com>
2021-07-27 14:57:29 -0400

    added open and close to tool

--- src/rosdiscover/recover/tool.py
+++ src/rosdiscover/recover/tool.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 __all__ = ('NodeRecoveryTool',)
 
+import contextlib
+import types
+import typing as t
+
+from loguru import logger
 import attr
 import roswire
 
-from .core import RecoveredNodeModel
 from ..config import Config
 
 
@@ -12,9 +16,30 @@ from ..config import Config
 class NodeRecoveryTool:
     _app: roswire.app.App
 
+    @contextlib.contextmanager
     @classmethod
     def for_config(cls, config: Config) -> 'NodeRecoveryTool':
-        return NodeRecoveryTool(app=config.app)
+        with NodeRecoveryTool(app=config.app) as tool:
+            yield tool
 
-    def recover(self, node_type: str, package: str) -> RecoveredNodeModel:
+    def __enter__(self) -> "NodeRecoveryTool":
+        self.open()
+        return self
+
+    def __exit__(
+        self,
+        ex_type: t.Optional[t.Type[BaseException]],
+        ex_val: t.Optional[BaseException],
+        ex_tb: t.Optional[types.TracebackType],
+    ) -> None:
+        self.close()
+        return
+
+    def open(self) -> None:
+        return
+
+    def close(self) -> None:
+        return
+
+    def recover(self, node_type: str, package: str) -> None:
         raise NotImplementedError
