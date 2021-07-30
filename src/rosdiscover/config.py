5d86d6249ab498379f217c0b46fcea77b3f388d1
Bradley Schmerl <schmerl@cs.cmu.edu>
2021-07-30 16:55:40 -0400

    Extend config file to include information about where to find sources. (#133)
    * Unresolved models will no longer be exceptional, so change this to allow more robust control flow.
    
    * Merged in changes to master.
    
    * Update config to allow explicit package source references.
    
    * Changed package_sources to node_sources
    
    * Changed node_source information to mapping
    
    * Made node_sources keyed by a tuple of package and node name
    
    * Fix to node_source
    
    * Review fixes
    
    * Review fixes

--- src/rosdiscover/config.py
+++ src/rosdiscover/config.py
@@ -2,12 +2,66 @@
 __all__ = ('Config',)
 
 from types import MappingProxyType
-from typing import Any, Mapping, Sequence
+import typing as t
 
 from loguru import logger
 import attr
 import roswire
 import yaml
+
+
+@attr.s(frozen=True, slots=True, auto_attribs=True)
+class NodeSourceInfo:
+    """
+    This is a class to represent nodes and their sources, as a bootstrap for recovering node information
+    from static analysis.
+
+    Note: This is an interim class, and will be replaced by CMakelists.txt analysis in the fullness of time.
+
+    Attributes
+    ----------
+    package_name: str
+        The name of the package where the node is from
+    node_name: str
+        The name of the node provided by the package
+    sources: Sequence[str]
+        The list of sources for building the node
+    """
+    package_name: str
+    node_name: str
+    sources: t.Sequence[str]
+
+    @classmethod
+    def from_dict(cls, dict_: t.Mapping[str, t.Any]) -> 'NodeSourceInfo':
+        """
+        Raises
+        ------
+        ValueError
+            If 'package' is undefined.
+        ValueError
+            If 'node' is undefined.
+        ValueError
+            If 'sources' is undefined.
+        """
+        if 'package' not in dict_:
+            raise ValueError("'package' is undefined for the node source.")
+        if 'node' not in dict_:
+            raise ValueError("'node' is undefined for the node source.")
+        if 'sources' not in dict_:
+            raise ValueError("'sources' is undefined for the node source.")
+
+        if not isinstance(dict_['package'], str):
+            raise ValueError("expected 'package' to be a string")
+        if not isinstance(dict_['node'], str):
+            raise ValueError("expected 'node' to be a string")
+        if not isinstance(dict_['sources'], list):
+            raise ValueError("expected 'sources' to be a list")
+
+        return NodeSourceInfo(
+            package_name=dict_['package'],
+            node_name=dict_['node'],
+            sources=list(dict_['sources']),
+        )
 
 
 @attr.s(frozen=True, slots=True, auto_attribs=True)
@@ -27,15 +81,18 @@ class Config:
         A set of environment variables that should be used by the application.
     app: roswire.app.App
         The ROSWire application for this configuration.
+    node_sources: List[NodeSourceInfo]
+        Provides instructions on how to statically recover the architecture code for individual nodes
     """
     image: str
-    sources: Sequence[str]
-    launches: Sequence[str]
-    environment: Mapping[str, str] = attr.ib(factory=dict)
+    sources: t.Sequence[str]
+    launches: t.Sequence[str]
+    environment: t.Mapping[str, str] = attr.ib(factory=dict)
+    node_sources: t.Mapping[t.Tuple[str, str], NodeSourceInfo] = attr.ib(factory=dict)
     app: roswire.app.App = attr.ib(init=False)
 
     @classmethod
-    def from_dict(cls, dict_: Mapping[str, Any]) -> 'Config':
+    def from_dict(cls, dict_: t.Mapping[str, t.Any]) -> 'Config':
         """
         Raises
         ------
@@ -62,14 +119,24 @@ class Config:
         if has_environment and not isinstance(dict_['environment'], dict):
             raise ValueError("expected 'environment' to be a mapping")
 
+        has_node_sources = 'node_sources' in dict_
+        if has_node_sources and not isinstance(dict_['node_sources'], list):
+            raise ValueError("expected 'node_sources' to be a list")
+
         image: str = dict_['image']
-        sources: Sequence[str] = dict_['sources']
-        launches: Sequence[str] = dict_['launches']
-        environment: Mapping[str, str] = dict(dict_.get('environment', {}))
+        sources: t.Sequence[str] = dict_['sources']
+        launches: t.Sequence[str] = dict_['launches']
+        environment: t.Mapping[str, str] = dict(dict_.get('environment', {}))
+        node_sources_list: t.Sequence[t.Dict[str, t.Any]] = list(dict_.get('node_sources', []))
+
+        node_sources = {(nsi.package_name, nsi.node_name): nsi
+                        for nsi in (NodeSourceInfo.from_dict(d)
+                                    for d in node_sources_list)}
         return Config(image=image,
                       sources=sources,
                       launches=launches,
-                      environment=environment)
+                      environment=environment,
+                      node_sources=node_sources)
 
     @classmethod
     def from_yaml_string(cls, yml: str) -> 'Config':
