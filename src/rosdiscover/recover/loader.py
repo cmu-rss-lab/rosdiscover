f51a98334b4093e6324d3363cab7f5bb75a847af
ChrisTimperley <christimperley@googlemail.com>
2021-08-13 14:38:03 -0400

    outlined more of the loader

--- src/rosdiscover/recover/loader.py
+++ src/rosdiscover/recover/loader.py
@@ -6,16 +6,31 @@ __all__ = ("SymbolicProgramLoader",)
 import typing as t
 
 from .symbolic import (
+    SymbolicCompound,
     SymbolicFunction,
+    SymbolicParameter,
     SymbolicProgram,
 )
 
 
 class SymbolicProgramLoader:
-    def _load_function(self, dict_: t.Mapping[str, t.Any]) -> SymbolicFunction:
+    def _load_parameter(self, dict_: t.Mapping[str, t.Any]) -> SymbolicParameter:
         raise NotImplementedError
 
+    def _load_compound(self, dict_: t.Mapping[str, t.Any]) -> SymbolicCompound:
+        raise NotImplementedError
+
+    def _load_function(self, dict_: t.Mapping[str, t.Any]) -> SymbolicFunction:
+        name: str = dict_["name"]
+        parameters = [self._load_parameter(d) for d in dict_["parameters"]]
+        body = self._load_compound(dict_["body"])
+        return SymbolicFunction.build(
+            name,
+            parameters,
+            body,
+        )
+
     def load(self, dict_: t.Mapping[str, t.Any]) -> SymbolicProgram:
-        functions = [self._load_function(d) for d in dict_["functions"]]
-        name_to_function = {function.name: function for function in functions}
-        return SymbolicProgram(name_to_function)
+        return SymbolicProgram.build(
+            self._load_function(d) for d in dict_["functions"]
+        )
