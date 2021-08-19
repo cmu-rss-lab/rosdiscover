933ce61b28aea12110f85d7c7c43dc814c33a214
ChrisTimperley <christimperley@googlemail.com>
2021-08-19 17:00:35 -0400

    mode [spawn_model]: add support for -gazebo_namespace command-line argument

--- src/rosdiscover/models/spawn_model.py
+++ src/rosdiscover/models/spawn_model.py
@@ -10,15 +10,6 @@ from ..interpreter import model
 
 @model('gazebo_ros', 'spawn_model')
 def spawn_model(c):
-    ns_gz = '/gazebo'
-    c.sub(f'{ns_gz}/model_states', 'gazebo_msgs/ModelStates')
-    c.use(f'{ns_gz}/unpause_physics', 'std_srvs/Empty')
-    c.use(f'{ns_gz}/delete_model', 'gazebo_msgs/DeleteModel')
-    c.use(f'{ns_gz}/spawn_urdf_model_client', 'gazebo_msgs/SpawnModel')
-    c.use(f'{ns_gz}/spawn_sdf_model_client', 'gazebo_msgs/SpawnModel')
-    c.use(f'{ns_gz}/spawn_sdf_model_client', 'gazebo_msgs/SpawnModel')
-    c.use(f'{ns_gz}/set_model_configuration', 'gazebo_msgs/SetModelConfiguration')
-
     parser = argparse.ArgumentParser('spawn_model')
     parser.add_argument('-urdf', action='store_true')
     parser.add_argument('-model', type=str)
@@ -32,7 +23,17 @@ def spawn_model(c):
     parser.add_argument('-P', type=float)
     parser.add_argument('-Y', type=float)
     parser.add_argument('-J', type=float)
+    parser.add_argument('-gazebo_namespace', default='/gazebo')
     args = parser.parse_args(c.args.split())
+
+    ns_gz = args.gazebo_namespace
+    c.sub(f'{ns_gz}/model_states', 'gazebo_msgs/ModelStates')
+    c.use(f'{ns_gz}/unpause_physics', 'std_srvs/Empty')
+    c.use(f'{ns_gz}/delete_model', 'gazebo_msgs/DeleteModel')
+    c.use(f'{ns_gz}/spawn_urdf_model_client', 'gazebo_msgs/SpawnModel')
+    c.use(f'{ns_gz}/spawn_sdf_model_client', 'gazebo_msgs/SpawnModel')
+    c.use(f'{ns_gz}/spawn_sdf_model_client', 'gazebo_msgs/SpawnModel')
+    c.use(f'{ns_gz}/set_model_configuration', 'gazebo_msgs/SetModelConfiguration')
 
     urdf_param_name = args.param
     logger.debug(f'spawning model using parameter [{urdf_param_name}]')
