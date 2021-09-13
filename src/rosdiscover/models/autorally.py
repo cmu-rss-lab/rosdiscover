0a99a8da05a6255e7dd1e774a2772a1c68198112
Bradley Schmerl <schmerl@cs.cmu.edu>
2021-09-13 14:16:18 -0400

    Changes to CMake with merged models. (#178)
    * Added lap_stats for autorally
    
    * Added version changes
    
    * Added pointgray camera
    
    * Fist version of rostopic (doesn't work yet)
    
    * Removed extra line
    
    * Long line
    
    * Added rostopic pub and echo
    
    * Added dynamic parameters and proper topics
    
    * Mark unfound nodes as placeholders
    
    * Fixed nodelet manager info
    
    * Better error reporting
    
    * Moved to roswire
    
    * Added handwritten nodes for autorally_gazebo python nodes
    
    * Hooked autorally into module
    
    * Flake 8
    
    * Updated roswire commit
    
    * Better error reporting.

---
+++ src/rosdiscover/models/autorally.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+from ..interpreter import model, NodeContext
+
+# Python models for autorally_gazebo
+# Derived from: https://github.com/AutoRally/autorally/tree/melodic-devel/autorally_gazebo/nodes
+
+
+@model('autorally_gazebo', 'autorally_controller.py')
+def autorally_controller(c: NodeContext) -> None:
+
+    c.read("~left_front_wheel/steering_link_name", "left_steering_link")
+    c.read("~right_front_wheel/steering_link_name", "right_steering_link")
+    left_steering_controller_name = \
+        c.read("~left_front_wheel/steering_controller_name", "left_steering_controller")
+    assert isinstance(left_steering_controller_name, str)
+    right_steering_controller_name = \
+        c.read("~right_front_wheel/steering_controller_name", "right_steering_controller")
+    assert isinstance(right_steering_controller_name, str)
+    c.read("~left_rear_wheel/link_name", "left_wheel")
+    c.read("~right_rear_wheel/link_name", "right_wheel")
+
+    left_front_axle_controller_name = \
+        c.read("~left_front_wheel/axle_controller_name")
+    assert isinstance(left_front_axle_controller_name, str)
+
+    right_front_axle_controller_name = \
+        c.read("~right_front_wheel/axle_controller_name")
+    assert isinstance(right_front_axle_controller_name, str)
+
+    left_rear_axle_controller_name = \
+        c.read("~left_rear_wheel/axle_controller_name")
+    assert isinstance(left_rear_axle_controller_name, str)
+
+    right_rear_axle_controller_name = \
+        c.read("~right_rear_wheel/axle_controller_name")
+    assert isinstance(right_rear_axle_controller_name, str)
+
+    c.read("~left_front_wheel/diameter", 1.0)
+    c.read("~right_front_wheel/diameter")
+    c.read("~left_rear_wheel/diameter")
+    c.read("~right_rear_wheel/diameter")
+
+    # https://github.com/AutoRally/autorally/blob/c2692f2970da6874ad9ddfeea3908adaf05b4b09/autorally_gazebo/nodes/autorally_controller.py#L258
+    chassis_command_priorities = \
+        c.read("~chassisCommandProirities", [])  # Note, misspelling is deliberate
+
+    shock_absorbers = c.read("~shock_absorbers", [])
+
+    c.read("~cmd_timeout", 0.5)
+    c.read("~publishing_frequency", 30.0)
+
+    c.sub("chassisCommand", "autorally_msgs/chassisCOmmand")
+    c.sub("runstop", "autorally_msgs/runstop")
+
+    c.pub(f"{left_steering_controller_name}/command", "std_msgs/Float64")
+    c.pub(f"{right_steering_controller_name}/command", "std_msgs/Float64")
+    c.pub(f"{left_front_axle_controller_name}/command", "std_msgs/Float64")
+    c.pub(f"{right_front_axle_controller_name}/command", "std_msgs/Float64")
+    c.pub(f"{left_rear_axle_controller_name}/command", "std_msgs/Float64")
+    c.pub(f"{right_rear_axle_controller_name}/command", "std_msgs/Float64")
+
+    assert isinstance(shock_absorbers, list)
+    for shocker in shock_absorbers:
+        assert isinstance(shocker, dict)
+        assert 'controller_name' in shocker
+        c.pub(f"{shocker['controller_name']}/command", "std_msgs/Float64")  # latched = True
+
+    c.pub("~wheelSpeeds", "autorally_msgs/wheelSpeeds")
+    c.pub("~chassisState", "autorally_msgs/chassisState")
+
+    assert isinstance(chassis_command_priorities, list)
+    for cmd in chassis_command_priorities:
+        c.sub(f"~/{cmd}/chassisCommand", "autorally_msgs/chassisCommand")
+
+    c.sub('~/joint_states', "sensor_msgs/JointState")
+
+    c.provide('~/list_controllers', "controller_manager_msgs/ListControllers")
+
+
+@model('autorally_gazebo', 'ground_truth_republisher')
+def ground_truth_republisher(c: NodeContext) -> None:
+    c.pub('/ground_truth/state', 'nav_msgs/Odometry')
+    c.sub('/ground_truth/state_raw', 'nav_msgs/Odometry')
