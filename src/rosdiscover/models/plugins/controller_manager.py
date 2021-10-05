5ff5b738a03fe659d478ea01ca6c87edc92d9ee4
Chris Timperley <christimperley@googlemail.com>
2021-10-05 13:53:20 -0400

    Added support for controller_manager plugins (fixes #202) (#203)
    * outlined API for controller_manager plugins
    
    * fixed issues discovered by mypy
    
    * added loading of controller manager plugins
    
    * added implementation of JointStateControllerPlugin
    
    * added model for diffdrivecontroller
    
    * fixed bad import
    
    * produce warning rather than exception
    
    * fixed bad method call and added missing publishers
    
    * added missing subscriber

---
+++ src/rosdiscover/models/plugins/controller_manager.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+__all__ = ("ControllerManagerPlugin",)
+
+import abc
+import typing as t
+
+import attr
+from loguru import logger
+from roswire.name import namespace, namespace_join
+from ...interpreter import Interpreter, ModelPlugin, NodeContext
+
+
+@attr.s(frozen=True, slots=True)
+class ControllerManagerPlugin(ModelPlugin, abc.ABC):
+    type_name: t.ClassVar[str]
+
+    controller_name: str = attr.ib()
+    controller_manager_node: str = attr.ib()
+
+    @classmethod
+    def from_type(
+        cls,
+        controller_type: str,
+        controller_name: str,
+        controller_manager_node: str,
+    ) -> ControllerManagerPlugin:
+        """Loads a controller manager plugin.
+
+        Parameters
+        ----------
+        controller_type: str
+            The type of the controller
+        controller_name: str
+            The name of the controller
+        controller_manager_node: str
+            The fully qualified name of the node that hosts the associated control manager
+        """
+        logger.debug(
+            f"loading controller_manager plugin [{controller_name}] "
+            f"with type [{controller_type}] "
+            f"via node [{controller_manager_node}]"
+        )
+
+        # find the class associated with this type
+        # TODO iterate over subclasses and inspect type_name
+        type_to_class: t.Mapping[str, t.Type[ControllerManagerPlugin]] = {
+            'joint_state_controller/JointStateController': JointStateControllerPlugin,
+            'diff_drive_controller/DiffDriveController': DiffDriveControllerPlugin,
+        }
+        cls = type_to_class[controller_type]
+
+        plugin = cls.build(controller_name, controller_manager_node)
+        logger.debug(f'loaded controller_manager plugin [{controller_name}]')
+        return plugin
+
+    @classmethod
+    def build(cls, controller_name: str, controller_manager_node: str) -> ControllerManagerPlugin:
+        return cls(
+            controller_name=controller_name,
+            controller_manager_node=controller_manager_node,
+        )
+
+    @property
+    def namespace(self) -> str:
+        """The associated namespace for this controller."""
+        return namespace_join(self.controller_manager_node, self.controller_name)
+
+    def load(self, interpreter: Interpreter) -> None:
+        context = interpreter.nodes[self.controller_manager_node]
+        self._load(interpreter, context)
+
+    @abc.abstractmethod
+    def _load(self, interpreter: Interpreter, context: NodeContext) -> None:
+        """Simulates the architectural effects of this controller.
+
+        Parameters
+        ----------
+        interpreter: Interpreter
+            provides access to the simulated architecture for the entire ROS system
+        context: NodeContext
+            provides access to the node context for the associated controller_manager node
+            that hosts this controller
+        """
+        ...
+
+
+@attr.s(auto_attribs=True, frozen=True, slots=True)
+class JointStateControllerPlugin(ControllerManagerPlugin):
+    def _load(self, interpreter: Interpreter, context: NodeContext) -> None:
+        root_ns = namespace(self.namespace)
+        context.read("joints", None)
+        context.read("publish_rate")
+        context.read("extra_joints")
+        context.pub(f"{root_ns}/joint_states", "sensor_msgs::JointState")
+
+
+@attr.s(auto_attribs=True, frozen=True, slots=True)
+class DiffDriveControllerPlugin(ControllerManagerPlugin):
+    def _load(self, interpreter: Interpreter, context: NodeContext) -> None:
+        # FIXME this is only a partial model!
+        # https://github.com/ros-controls/ros_controllers/blob/noetic-devel/diff_drive_controller/src/diff_drive_controller.cpp
+
+        # TODO get wheel names
+
+        context.read("publish_rate", 50.0)
+        context.read("open_loop", False)
+        context.read("wheel_separation_multiplier", 1.0)
+
+        if context.has_param("wheel_radius_multiplier"):
+            context.read("wheel_radius_multiplier")
+        else:
+            context.read("left_wheel_radius_multiplier", 1.0)
+            context.read("right_wheel_radius_multiplier", 1.0)
+
+        context.read("velocity_rolling_window_size", 10)
+        context.read("cmd_vel_timeout", 0.5)
+        context.read("allow_multiple_cmd_vel_publishers", True)
+
+        context.read("base_frame_id", "base_link")
+        context.read("odom_frame_id", "odom")
+        context.read("enable_odom_tf", True)
+
+        context.read("linear/x/has_velocity_limits", False)
+        context.read("linear/x/has_acceleration_limits", False)
+        context.read("linear/x/has_jerk_limits", False)
+        context.read("linear/x/max_velocity", 0.0)
+        context.read("linear/x/min_velocity", 0.0)
+        context.read("linear/x/max_acceleration", 0.0)
+        context.read("linear/x/min_acceleration", 0.0)
+        context.read("linear/x/max_jerk", 0.0)
+        context.read("linear/x/min_jerk", 0.0)
+
+        context.read("angular/z/has_velocity_limits", False)
+        context.read("angular/z/has_acceleration_limits", False)
+        context.read("angular/z/has_jerk_limits", False)
+        context.read("angular/z/max_velocity", 0.0)
+        context.read("angular/z/min_velocity", 0.0)
+        context.read("angular/z/max_acceleration", 0.0)
+        context.read("angular/z/min_acceleration", 0.0)
+        context.read("angular/z/max_jerk", 0.0)
+        context.read("angular/z/min_jerk", 0.0)
+
+        should_publish_command = context.read("publish_cmd", False)
+        should_publish_joint_state = context.read("publish_wheel_joint_controller_state", False)
+
+        context.read("wheel_separation", 0.0)
+        context.read("wheel_radius", 0.0)
+
+        context.sub("cmd_vel", "geometry_msgs/Twist")
+
+        if should_publish_command:
+            context.pub("cmd_vel_out", "geometry_msgs/TwistStamped")
+
+        if should_publish_joint_state:
+            context.pub("wheel_joint_controller_state", "control_msgs/JointTrajectoryControllerState")
+
+        context.pub("odom", "nav_msgs/Odometry")
+        context.pub("/tf", "tf/tfMessage")
+
+        # TODO this node supports dynamic reconfiguration
+        logger.warning("plugin model is likely incomplete: DiffDriveControllerPlugin")
--- src/rosdiscover/models/spawner.py
+++ src/rosdiscover/models/spawner.py
@@ -1,9 +1,59 @@
 # -*- coding: utf-8 -*-
-from loguru import logger
+import argparse
+import os
+import typing as t
 
+from loguru import logger
+from roswire.name import namespace_join
+
+from .plugins.controller_manager import ControllerManagerPlugin
 from ..interpreter import model
 
 
 @model('controller_manager', 'spawner')
 def spawner(c):
-    logger.warning("Missing handwritten model for controller_manager/spawner")
+    parser = argparse.ArgumentParser("spawner")
+    # FIXME this is not quite how this argument works:
+    # https://github.com/ros-controls/ros_control/blob/5db3baaa71c9dcd8a2fadb3b2c0b5085ea49b3a1/controller_manager/scripts/spawner#L174-L185
+    parser.add_argument("--namespace", default="/")
+    parser.add_argument("controllers", nargs="+")
+    args = parser.parse_args(c.args.split())
+
+    controllers_to_spawn: t.List[str] = []
+    for controller_name_or_filename in args.controllers:
+        if os.path.isabs(controller_name_or_filename):
+            m = "no support for spawners that load their controllers from a config file"
+            raise NotImplementedError(m)
+        else:
+            controllers_to_spawn.append(controller_name_or_filename)
+
+    # the spawner interacts with the controller_manager services
+    load_controller_service = namespace_join(args.namespace, "controller_manager/load_controller")
+    unload_controller_service = namespace_join(args.namespace, "controller_manager/unload_controller")
+    switch_controller_service = namespace_join(args.namespace, "controller_manager/switch_controller")
+
+    c.use(load_controller_service, "controller_manager_msgs/LoadController")
+    c.use(unload_controller_service, "controller_manager_msgs/UnloadController")
+    c.use(switch_controller_service, "controller_manager_msgs/SwitchController")
+
+    # FIXME load each controller as a plugin for the associated controller_manager
+    # this is a little tricky since we need to find the correct controller_manager
+    # going forward, we can (after solving some ordering/dependency issues) iterate
+    # over the system to find the node that provides the necessary services
+    #
+    # for now, I'm hardcoding gazebo so that we crack on with our experiments
+    controller_manager_node = "/gazebo"
+    controller_manager_namespace = "/"
+
+    for controller_name in controllers_to_spawn:
+        logger.debug(f"finding type for controller [{controller_name}]")
+        controller_namespace = namespace_join(controller_manager_namespace, controller_name)
+        controller_type = c.read(f"{controller_namespace}/type")
+        logger.debug(f"found type for controller [{controller_name}]: {controller_type}")
+
+        plugin = ControllerManagerPlugin.build(
+            controller_name=controller_name,
+            controller_type=controller_type,
+            controller_manager_node=controller_manager_node,
+        )
+        c.load_plugin(plugin)
