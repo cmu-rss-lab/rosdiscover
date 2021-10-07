1fbd155150e2128941897492a9abfce5bebcacc1
Bradley Schmerl <schmerl@cs.cmu.edu>
2021-10-07 17:50:19 -0400

    Deal with <robot> and <robot ...

--- src/rosdiscover/models/spawn_model.py
+++ src/rosdiscover/models/spawn_model.py
@@ -46,14 +46,17 @@ def spawn_model(c):
     #  Workaround to find the <robot> begin tag
     begin_tag = "<robot>"
     begin_tag_starts_at = urdf_contents.find(begin_tag)
-    urdf_contents = urdf_contents[begin_tag_starts_at:]
+    if begin_tag_starts_at == -1:
+        begin_tag_starts_at.find("<robot ")
+    if begin_tag_starts_at != -1:
+        urdf_contents = urdf_contents[begin_tag_starts_at:]
 
     # #94 workaround to deal with inclusion of warning when xacro.py is used
     end_tag = '</robot>'
     end_tag_starts_at = urdf_contents.rfind(end_tag)
     end_tag_ends_at = end_tag_starts_at + len(end_tag)
     urdf_contents = urdf_contents[:end_tag_ends_at]
-    logger.debug(urdf_param_name)
+    logger.debug("New contents are:")
     logger.debug(urdf_contents)
     urdf_xml = ET.fromstring(urdf_contents)
     for plugin_xml in urdf_xml.findall('.//plugin'):
