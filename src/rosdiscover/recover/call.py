c56d5758612a3a95974bf495666b94a55541b7e0
ChrisTimperley <christimperley@googlemail.com>
2021-10-19 20:45:08 -0400

    added is_unknown to SymbolicApiCall API

--- src/rosdiscover/recover/call.py
+++ src/rosdiscover/recover/call.py
@@ -29,6 +29,10 @@ from .symbolic import (
 
 class SymbolicRosApiCall(SymbolicStatement, abc.ABC):
     """Represents a symbolic call to a ROS API."""
+    @abc.abstractmethod
+    def is_unknown(self) -> bool:
+        """Determines whether the target of this API call is unknown."""
+        ...
 
 
 @attr.s(frozen=True, auto_attribs=True, slots=True)
@@ -43,6 +47,9 @@ class RosInit(SymbolicRosApiCall):
 
     def eval(self, context: SymbolicContext) -> None:
         return None
+
+    def is_unknown(self) -> bool:
+        return self.name.is_unknown()
 
 
 @attr.s(frozen=True, auto_attribs=True, slots=True)
@@ -61,6 +68,9 @@ class Publisher(SymbolicRosApiCall):
         topic = self.topic.eval(context)
         context.node.pub(topic, self.format_)
 
+    def is_unknown(self) -> bool:
+        return self.topic.is_unknown()
+
 
 @attr.s(frozen=True, auto_attribs=True, slots=True)
 class Subscriber(SymbolicRosApiCall):
@@ -77,6 +87,9 @@ class Subscriber(SymbolicRosApiCall):
     def eval(self, context: SymbolicContext) -> None:
         topic = self.topic.eval(context)
         context.node.sub(topic, self.format_)
+
+    def is_unknown(self) -> bool:
+        return self.topic.is_unknown()
 
 
 @attr.s(frozen=True, auto_attribs=True, slots=True)
@@ -95,6 +108,9 @@ class ServiceProvider(SymbolicRosApiCall):
         service = self.service.eval(context)
         context.node.provide(service, self.format_)
 
+    def is_unknown(self) -> bool:
+        return self.service.is_unknown()
+
 
 @attr.s(frozen=True, auto_attribs=True, slots=True)
 class ServiceCaller(SymbolicRosApiCall):
@@ -111,6 +127,9 @@ class ServiceCaller(SymbolicRosApiCall):
     def eval(self, context: SymbolicContext) -> None:
         service = self.service.eval(context)
         context.node.use(service, self.format_)
+
+    def is_unknown(self) -> bool:
+        return self.service.is_unknown()
 
 
 @attr.s(frozen=True, auto_attribs=True, slots=True)
@@ -130,6 +149,11 @@ class WriteParam(SymbolicRosApiCall):
         value = self.value.eval(context)
         context.node.write(param, value)
 
+    def is_unknown(self) -> bool:
+        # NOTE we don't consider the parameter value
+        # we should discuss whether or not we should
+        return self.param.is_unknown()
+
 
 @attr.s(frozen=True, auto_attribs=True, slots=True)
 class ReadParam(SymbolicRosApiCall, SymbolicValue):
@@ -144,6 +168,9 @@ class ReadParam(SymbolicRosApiCall, SymbolicValue):
     def eval(self, context: SymbolicContext) -> None:
         param = self.param.eval(context)
         context.node.read(param)
+
+    def is_unknown(self) -> bool:
+        return self.param.is_unknown()
 
 
 @attr.s(frozen=True, auto_attribs=True, slots=True)
@@ -163,6 +190,10 @@ class ReadParamWithDefault(SymbolicRosApiCall, SymbolicValue):
         default = self.default.eval(context)
         context.node.read(param, default)
 
+    def is_unknown(self) -> bool:
+        # NOTE same comment about default
+        return self.param.is_unknown()
+
 
 @attr.s(frozen=True, auto_attribs=True, slots=True)
 class HasParam(SymbolicRosApiCall, SymbolicBool):
@@ -178,6 +209,9 @@ class HasParam(SymbolicRosApiCall, SymbolicBool):
         param = self.param.eval(context)
         context.node.has_param(param)
 
+    def is_unknown(self) -> bool:
+        return self.param.is_unknown()
+
 
 @attr.s(frozen=True, auto_attribs=True, slots=True)
 class DeleteParam(SymbolicRosApiCall):
@@ -192,3 +226,6 @@ class DeleteParam(SymbolicRosApiCall):
     def eval(self, context: SymbolicContext) -> None:
         param = self.param.eval(context)
         context.node.delete_param(param)
+
+    def is_unknown(self) -> bool:
+        return self.param.is_unknown()
