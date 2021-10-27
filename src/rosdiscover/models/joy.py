adc2fc1d57529c3995d5582928184aceb232049d
Bradley Schmerl <schmerl@cs.cmu.edu>
2021-10-27 15:25:59 -0400

    Remove joy_node and robot_state_publisher

--- src/rosdiscover/models/joy.py
+++ src/rosdiscover/models/joy.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-from ..interpreter import model
+#from ..interpreter import model
 
 
-@model('joy', 'joy_node')
+#@model('joy', 'joy_node')
 def joy(c):
     c.pub('diagnostics', 'diagnostic_msgs/DiagnosticArray')
     c.pub('joy', 'sensor_msgs/Joy')
