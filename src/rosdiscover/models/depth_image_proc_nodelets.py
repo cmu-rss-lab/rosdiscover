fbc10e313136de55d05dd482e484a60bf1da0a6f
Bradley Schmerl <schmerl@cs.cmu.edu>
2021-11-02 18:26:42 -0400

    camera name resolution

--- src/rosdiscover/models/depth_image_proc_nodelets.py
+++ src/rosdiscover/models/depth_image_proc_nodelets.py
@@ -11,16 +11,16 @@ DEPTH_IMAGE_PROC_PKG = 'depth_image_proc'
 def convert_metric(c):
     c.mark_nodelet()
 
-    c.sub('image_raw', IMAGE_TOPIC_TYPE)
-    c.pub('image', IMAGE_TOPIC_TYPE)
+    c.sub('~image_raw', IMAGE_TOPIC_TYPE)
+    c.pub('~image', IMAGE_TOPIC_TYPE)
 
 
 @model(DEPTH_IMAGE_PROC_PKG, 'disparity')
 def disparity(c):
     c.mark_nodelet()
 
-    c.sub('left/image_rect', IMAGE_TOPIC_TYPE)
-    c.sub('right/camera_info', MSGS_CAMERA_INFO)
+    c.sub('~left/image_rect', IMAGE_TOPIC_TYPE)
+    c.sub('~right/camera_info', MSGS_CAMERA_INFO)
 
     c.read('min_range', 0.0)
     c.read('max_range', 0.0)
@@ -32,10 +32,10 @@ def disparity(c):
 def point_cloud_xyz(c):
     c.mark_nodelet()
 
-    c.sub('camera_info', MSGS_CAMERA_INFO)
-    c.sub('image_rect', IMAGE_TOPIC_TYPE)
+    c.sub('~camera_info', MSGS_CAMERA_INFO)
+    c.sub('~image_rect', IMAGE_TOPIC_TYPE)
 
-    c.pub('points', POINTCLOUD2)
+    c.pub('~points', POINTCLOUD2)
 
     c.read('queue_size', 5)
 
@@ -44,11 +44,11 @@ def point_cloud_xyz(c):
 def point_cloud_xyzrgb(c):
     c.mark_nodelet()
 
-    c.sub('rgb/camera_info', MSGS_CAMERA_INFO)
-    c.sub('rgb/image_rect_color', IMAGE_TOPIC_TYPE)
-    c.sub('depth_registered/image_rect', IMAGE_TOPIC_TYPE)
+    c.sub('~rgb/camera_info', MSGS_CAMERA_INFO)
+    c.sub('~rgb/image_rect_color', IMAGE_TOPIC_TYPE)
+    c.sub('~depth_registered/image_rect', IMAGE_TOPIC_TYPE)
 
-    c.pub('depth_registered/points', POINTCLOUD2)
+    c.pub('~depth_registered/points', POINTCLOUD2)
 
     c.read('queue_size', 5)
 
@@ -57,14 +57,14 @@ def point_cloud_xyzrgb(c):
 def register(c):
     c.mark_nodelet()
 
-    c.sub('rgb/camera_info', MSGS_CAMERA_INFO)
-    c.sub('depth/camera_info', MSGS_CAMERA_INFO)
-    c.sub('depth/image_rect', IMAGE_TOPIC_TYPE)
+    c.sub('~rgb/camera_info', MSGS_CAMERA_INFO)
+    c.sub('~depth/camera_info', MSGS_CAMERA_INFO)
+    c.sub('~depth/image_rect', IMAGE_TOPIC_TYPE)
 
-    c.pub('depth_registered/camera_info', MSGS_CAMERA_INFO)
-    c.pub('depth_registered/image_rect', IMAGE_TOPIC_TYPE)
+    c.pub('~depth_registered/camera_info', MSGS_CAMERA_INFO)
+    c.pub('~depth_registered/image_rect', IMAGE_TOPIC_TYPE)
 
-    c.read('queue_size', 5)
+    c.read('~queue_size', 5)
 
     # TODO: The documentation mentions that this TF should exist
     # but we don't have a way to record it
