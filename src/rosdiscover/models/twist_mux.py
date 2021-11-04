4f19c18c3200940f089cacc09fbd82bdb8170d68
Bradley Schmerl <schmerl@cs.cmu.edu>
2021-11-03 20:08:46 -0400

    Removed joy_node from handwritten models, fixed twist_mux (#236)

--- src/rosdiscover/models/twist_mux.py
+++ src/rosdiscover/models/twist_mux.py
@@ -4,10 +4,10 @@ from ..interpreter import model
 
 @model('twist_mux', 'twist_mux')
 def twist_mux(c):
-    c.pub('cmd_vel_out', 'geometry_msgs/Twist')
+    topics = c.read("~topics", [])
+    locks = c.read("~locks", [])
+    c.pub('~cmd_vel', 'geometry_msgs/Twist')
     c.pub('diagnostics', 'diagnostic_msgs/DiagnosticArray')
-    c.sub('/clock', 'rosgraph_msgs/Clock')
-    c.sub('cmd_vel', 'unknown type')
-    c.sub('e_stop', 'unknown type')
-    c.sub('/move_base/cmd_vel', 'unknown type')
-    c.sub('/pad_teleop/cmd_vel', 'unknown type')
+
+    for topic in topics + locks:
+        c.sub(f"/{topic['topic']}", "any")
