4e728d00568989916ff0fc5eb8a8fd851d14a296
Bradley Schmerl <schmerl@cs.cmu.edu>
2021-11-06 15:30:47 -0400

    Added code for error reproduction.

---
+++ src/rosdiscover/models/rosdiscover_error_reproducer.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from ..interpreter import model, NodeContext
+
+@model('*', 'error_reproducer')
+def error_reproducer(c: NodeContext):
+    topics = c.read("~topic", [])
+    for t in topics:
+        if t["direction"] == 'pub':
+            c.pub(t["name"], t["type"])
+        elif t["direction"] == 'sub':
+            c.sub(t["name"], t["type"])
