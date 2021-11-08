82859662ba51816bba9804d7e0c461de20f1562e
ChrisTimperley <christimperley@googlemail.com>
2021-11-08 14:03:50 -0500

    added _repair_urdf_contents

--- src/rosdiscover/models/spawn_model.py
+++ src/rosdiscover/models/spawn_model.py
@@ -12,27 +12,30 @@ from .plugins.gazebo import GazeboPlugin
 from ..interpreter import model
 
 
+def _repair_urdf_contents(contents: str) -> str:
+    #  Workaround to find the <robot> begin tag
+    begin_tag = "<robot>"
+    begin_tag_starts_at = contents.find(begin_tag)
+    if begin_tag_starts_at == -1:
+        begin_tag_starts_at = contents.find("<robot ")
+    if begin_tag_starts_at != -1:
+        contents = contents[begin_tag_starts_at:]
+
+    # #94 workaround to deal with inclusion of warning when xacro.py is used
+    end_tag = '</robot>'
+    end_tag_starts_at = contents.rfind(end_tag)
+    end_tag_ends_at = end_tag_starts_at + len(end_tag)
+    contents = contents[:end_tag_ends_at]
+    logger.debug(f"New contents are:\n{contents}")
+    return contents
+
+
 def _load_urdf_xml_from_parameter(c, parameter_name: str) -> t.Optional[ET.ElementTree]:
     logger.debug(f'spawning model using parameter [{parameter_name}]')
     urdf_contents = c.read(parameter_name).strip()
     logger.debug(f'parsing URDF model from parameter [{parameter_name}]:'
                  f'\n{urdf_contents}')
-
-    #  Workaround to find the <robot> begin tag
-    begin_tag = "<robot>"
-    begin_tag_starts_at = urdf_contents.find(begin_tag)
-    if begin_tag_starts_at == -1:
-        begin_tag_starts_at = urdf_contents.find("<robot ")
-    if begin_tag_starts_at != -1:
-        urdf_contents = urdf_contents[begin_tag_starts_at:]
-
-    # #94 workaround to deal with inclusion of warning when xacro.py is used
-    end_tag = '</robot>'
-    end_tag_starts_at = urdf_contents.rfind(end_tag)
-    end_tag_ends_at = end_tag_starts_at + len(end_tag)
-    urdf_contents = urdf_contents[:end_tag_ends_at]
-    logger.debug("New contents are:")
-    logger.debug(urdf_contents)
+    urdf_contents = _repair_urdf_contents(contents)
     return ET.fromstring(urdf_contents)
 
 
@@ -44,6 +47,7 @@ def _load_urdf_xml_from_file(c, filename: str) -> t.Optional[ET.ElementTree]:
     logger.debug(f"loading URDF XML from file [{filename}]")
     urdf_contents = c.read_file(filename)
     logger.debug(f"loaded URDF XML contents from file [{filename}]:\n{urdf_contents}")
+    urdf_contents = _repair_urdf_contents(contents)
     logger.debug("parsing URDF XML contents")
     xml = ET.fromstring(urdf_contents)
     logger.debug("parsed UDF XML contents")
