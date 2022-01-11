1bf4bc2c874c39aee0bf3a036c60e62d22c487ab
Bradley Schmerl <schmerl@cs.cmu.edu>
2022-01-11 11:24:30 -0500

    Use bash explicitly.

--- src/rosdiscover/observer/observer.py
+++ src/rosdiscover/observer/observer.py
@@ -93,7 +93,7 @@ class Observer(ABC):
             try:
                 if start_script:
                     logger.debug("Starting the container")
-                    cmd = start_script
+                    cmd = f"/bin/bash {start_script}"
                     script = instance.shell.popen(cmd)
                     time.sleep(5)
                     if script.returncode and script.returncode != 1:
