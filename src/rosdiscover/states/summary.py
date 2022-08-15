c0b1cac9a2c69f51db80561a0ac1c1517c6c7c10
Tobias Dürschmid <duerschmid@cmu.edu>
2022-08-15 14:59:51 -0400

    Add to_dict to states summary

--- src/rosdiscover/states/summary.py
+++ src/rosdiscover/states/summary.py
@@ -12,11 +12,19 @@ class StateVariable:
     name: str
     type: SymbolicValueType
 
+    def to_dict(self):
+        return {'name': self.name,
+                'type': self.type}
 
 @attr.s(frozen=True, slots=True, auto_attribs=True)
 class State:
     """Describes a concrete state of a node"""
     variables: t.Dict[StateVariable, SymbolicValue]
+
+    def to_dict(self):
+        vars = [{'variable': k.to_dict(), 
+                'value': v.to_dict()} for (k, v) in self.variables.keys]
+        return {'variables': vars}
 
 
 @attr.s(frozen=True, slots=True, auto_attribs=True)
@@ -24,3 +32,8 @@ class StateMachineSummary:
     """Summarises the state machine of a given node."""
     initial: State
     states: t.Set[State]
+
+    def to_dict(self) -> t.Dict[str, t.Any]:
+        states = [s.to_dict() for s in self.states]
+        return {'states': states,
+                'initial': self.initial.to_dict()}
