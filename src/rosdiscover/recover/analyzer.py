e434839082575994a386f2a95bed6d8034f652de
Tobias Dürschmid <duerschmid@cmu.edu>
2022-08-15 14:43:51 -0400

    Use cached properties

--- src/rosdiscover/recover/analyzer.py
+++ src/rosdiscover/recover/analyzer.py
@@ -8,6 +8,7 @@ __all__ = (
 import typing as t
 
 import attr
+from functools import cached_property
 
 from .symbolic import (
     SymbolicProgram,
@@ -20,85 +21,87 @@ from .call import Publish, RateSleep
 from .call import Subscriber
 
 
-@attr.s
+@attr.s(auto_attribs=True, slots=True)
 class SymbolicProgramAnalyzer:
 
-    @classmethod
-    def subscribers(cls, program: SymbolicProgram) -> t.Set[Subscriber]:
+    program: SymbolicProgram
+
+    @cached_property
+    def subscribers(self) -> t.Set[Subscriber]:
         result = set()
-        for func in program.functions.values():
+        for func in self.program.functions.values():
             for stmt in func.body:
                 if isinstance(stmt, Subscriber):
                     result.add(stmt)
 
         return result
 
-    @classmethod
-    def rate_sleeps(cls, program: SymbolicProgram) -> t.Set[RateSleep]:
+    @cached_property
+    def rate_sleeps(self) -> t.Set[RateSleep]:
         result = set()
-        for func in program.functions.values():
+        for func in self.program.functions.values():
             for stmt in func.body:
                 if isinstance(stmt, RateSleep):
                     result.add(stmt)
 
         return result
 
-    @classmethod
-    def subscriber_callbacks(cls, program: SymbolicProgram) -> t.Set[SymbolicFunction]:
+    @cached_property
+    def subscriber_callbacks(self) -> t.Set[SymbolicFunction]:
         result = set()
-        for sub in cls.subscribers(program):
+        for sub in self.subscribers:
             if sub.callback_name == "unknown":
                 continue
-            result.add(program.functions[sub.callback_name])
+            result.add(self.program.functions[sub.callback_name])
 
         return result
 
-    @classmethod
-    def publish_calls(cls, program: SymbolicProgram) -> t.List[Publish]:
+    @cached_property
+    def publish_calls(self) -> t.List[Publish]:
         result = []
-        for func in program.functions.values():
+        for func in self.program.functions.values():
             for stmt in func.body:
                 if isinstance(stmt, Publish) and stmt not in result:
                     result.append(stmt)
 
         return result
 
-    @classmethod
-    def publish_calls_in_sub_callback(cls, program: SymbolicProgram) -> t.List[Publish]:
+    @cached_property
+    def publish_calls_in_sub_callback(self) -> t.List[Publish]:
         result = []
-        for pub_call in cls.publish_calls(program):
-            for callback in cls.subscriber_callbacks(program):
-                if callback.body.contains(pub_call, program.functions) and pub_call not in result:
+        for pub_call in self.publish_calls:
+            for callback in self.subscriber_callbacks:
+                if callback.body.contains(pub_call, self.program.functions) and pub_call not in result:
                     result.append(pub_call)
 
         return result
 
-    @classmethod
-    def while_loops(cls, program: SymbolicProgram) -> t.List[SymbolicWhile]:
+    @cached_property
+    def while_loops(self) -> t.List[SymbolicWhile]:
         result = []
-        for func in program.functions.values():
+        for func in self.program.functions.values():
             for stmt in func.body:
                 if isinstance(stmt, SymbolicWhile) and stmt not in result:
                     result.append(stmt)
 
         return result
 
-    @classmethod
-    def periodic_publish_calls(cls, program: SymbolicProgram) -> t.List[Publish]:
+    @cached_property
+    def periodic_publish_calls(self) -> t.List[Publish]:
         result = []
-        for pub_call in cls.publish_calls(program):
-            for while_stmt in cls.while_loops(program):
-                if while_stmt.body.contains(pub_call, program.functions):
-                    for rate in cls.rate_sleeps(program):
-                        if while_stmt.body.contains(rate, program.functions) and pub_call not in result:
+        for pub_call in self.publish_calls:
+            for while_stmt in self.while_loops:
+                if while_stmt.body.contains(pub_call, self.program.functions):
+                    for rate in self.rate_sleeps:
+                        if while_stmt.body.contains(rate, self.program.functions) and pub_call not in result:
                             result.append(pub_call)
 
         return result
 
-    @classmethod
-    def function_calls(cls, program: SymbolicProgram) -> t.List[SymbolicFunctionCall]:
+    @cached_property
+    def function_calls(self) -> t.List[SymbolicFunctionCall]:
         result = []
-        for func in program.functions.values():
+        for func in self.program.functions.values():
             for stmt in func.body:
                 if isinstance(stmt, SymbolicFunctionCall) and stmt not in result:
                     result.append(stmt)
