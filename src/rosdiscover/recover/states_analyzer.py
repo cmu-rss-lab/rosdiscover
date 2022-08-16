221802ed55c43071a09152847e512bc334c8e9bf
Tobias Dürschmid <duerschmid@cmu.edu>
2022-08-16 10:44:13 -0400

    Add state_vars to SymbolicStatesAnalyzer

--- src/rosdiscover/recover/states_analyzer.py
+++ src/rosdiscover/recover/states_analyzer.py
@@ -5,10 +5,14 @@ __all__ = (
     "SymbolicStatesAnalyzer"
 )
 
+import typing as t
 import attr
+from functools import cached_property
 
 from .symbolic import (
     SymbolicProgram,
+    SymbolicValue,
+    SymbolicVariableReference,
 )
 from .analyzer import SymbolicProgramAnalyzer
 
@@ -18,3 +22,18 @@ class SymbolicStatesAnalyzer:
 
     program: SymbolicProgram
     program_analyzer: SymbolicProgramAnalyzer
+
+    @cached_property
+    def state_vars(self) -> t.List[SymbolicVariableReference]:
+        var_refs: t.List[SymbolicVariableReference] = []
+        for pub in self.program_analyzer.publish_calls:
+            for expr in pub.condition.decendents():
+                if isinstance(expr, SymbolicVariableReference):
+                    var_refs.append(expr)
+
+        return var_refs
+
+    @cached_property
+    def transitions(self) -> t.List[t.Any]:
+        result: t.List[t.Any] = []
+        return result
