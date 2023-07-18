dda657ca0f7d387648cd365ea69185c6f0145ad4
Tobias Dürschmid <duerschmid@cmu.edu>
2023-07-17 20:30:56 -0400

    fix tests

--- tests/test_behavior_analysis.py
+++ tests/test_behavior_analysis.py
@@ -62,13 +62,13 @@ class TestStringMethods(unittest.TestCase):
         self.assertSetEqual(rate_sleeps, sleeps)        
 
     def test_velocity_set(self):
-        model = self.get_model(self.autoware_file, "astar_planner", "velocity_set")
+        analyzer = SymbolicProgramAnalyzer(self.get_model(self.autoware_file, "astar_planner", "velocity_set").program)
 
-        self.assert_publish_calls_in_sub_callback(model,set())
+        self.assert_publish_calls_in_sub_callback(analyzer,set())
 
-        self.assert_rate_sleeps(model, {10.0})
+        self.assert_rate_sleeps(analyzer, {10.0})
 
-        self.assert_sub_callbacks(model, 
+        self.assert_sub_callbacks(analyzer, 
             {
                 "VelocitySetPath::waypointsCallback", 
                 "VelocitySetPath::currentVelocityCallback",
@@ -85,7 +85,7 @@ class TestStringMethods(unittest.TestCase):
             }
         )
 
-        self.assert_periodic_publish_calls(model,
+        self.assert_periodic_publish_calls(analyzer,
             {
                 "obstacle_pub",
                 "obstacle_waypoint_pub",
@@ -119,7 +119,7 @@ class TestStringMethods(unittest.TestCase):
 
         self.assert_publish_calls(
             analyzer,
-            {'odometry_publisher_', 'velocity_publisher_'}
+            {'tf_broadcaster', 'odometry_publisher_', 'velocity_publisher_'}
         )
 
         self.assert_rate_sleeps(analyzer, {50.0})
@@ -138,6 +138,7 @@ class TestStringMethods(unittest.TestCase):
 
         self.assert_periodic_publish_calls(analyzer,
             {
+                "tf_broadcaster",
                 "odometry_publisher_",
                 "velocity_publisher_",
             }
